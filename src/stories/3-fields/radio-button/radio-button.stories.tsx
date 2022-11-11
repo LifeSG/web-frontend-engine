9c1622cc55dc11da354f9ad3cff0f71eb55285e3
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-11-11 17:26:44 +0800

    [RADIO][JL] Create radio button storybook

---
+++ src/stories/3-fields/radio-button/radio-button.stories.tsx
@@ -0,0 +1,105 @@
+import { ArgsTable, Description, Heading, PRIMARY_STORY, Stories, Title } from "@storybook/addon-docs";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { FrontendEngine } from "../../../components";
+import { IRadioButtonGroupSchema } from "../../../components/fields/radio-button/types";
+import { CommonFieldStoryProps, ExcludeReactFormHookProps, SubmitButtonStorybook } from "../../common";
+
+export default {
+	title: "Field/RadioButton",
+	parameters: {
+		docs: {
+			page: () => (
+				<>
+					<Title>Radio Button</Title>
+					<Description>This component provides a set of radio buttons for user to select</Description>
+					<Heading>Props</Heading>
+					<ArgsTable story={PRIMARY_STORY} />
+					<Stories includePrimary={true} title="Examples" />
+				</>
+			),
+		},
+	},
+	argTypes: {
+		...ExcludeReactFormHookProps,
+		...CommonFieldStoryProps("radio"),
+		"radio-button-default": { table: { disable: true } },
+		disabled: {
+			description: "Specifies if the radio buttons should be disabled",
+			table: {
+				type: {
+					summary: "boolean",
+				},
+			},
+			options: [true, false],
+			control: {
+				type: "boolean",
+			},
+			defaultValue: false,
+		},
+		options: {
+			description: "A list of options that a user can choose from",
+			table: {
+				type: {
+					summary: "IOption[]",
+				},
+			},
+			type: { name: "object", value: {} },
+		},
+		label: {
+			description: "Specifies the label text",
+			table: {
+				type: {
+					summary: "string",
+				},
+			},
+			control: {
+				type: "text",
+			},
+		},
+	},
+} as Meta;
+
+const Template: Story<Record<string, IRadioButtonGroupSchema>> = (args) => (
+	<FrontendEngine data={{ fields: { ...args, ...SubmitButtonStorybook } }} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+	"radio-button-default": {
+		fieldType: "radio",
+		label: "Radio Button",
+		options: [
+			{ label: "Apple", value: "Apple" },
+			{ label: "Berry", value: "Berry" },
+			{ label: "Cherry", value: "Cherry" },
+		],
+	},
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	"radio-button-disabled": {
+		fieldType: "radio",
+		label: "Radio Button",
+		options: [
+			{ label: "Apple", value: "Apple" },
+			{ label: "Berry", value: "Berry" },
+			{ label: "Cherry", value: "Cherry" },
+		],
+		disabled: true,
+	},
+};
+
+export const WithValidation = Template.bind({});
+WithValidation.args = {
+	"radio-button-with-validation": {
+		fieldType: "radio",
+		label: "Radio Button",
+		options: [
+			{ label: "Apple", value: "Apple" },
+			{ label: "Berry", value: "Berry" },
+			{ label: "Cherry", value: "Cherry" },
+		],
+		validation: [{ required: true }],
+	},
+};
