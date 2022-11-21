a2eec79715456294563bfa25342b13291b2bd6ef
Wei Li <ng_wei_li@tech.gov.sg>
2022-11-21 17:49:39 +0800

    [TESTS][WL] create unit tests for textfield component

---
+++ src/__tests__/components/fields/textfield/textfield.spec.tsx
@@ -0,0 +1,174 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { FrontendEngine } from "../../../../components";
+import { IEmailSchema, INumberSchema, ITextfieldSchema } from "../../../../components/fields";
+import { IFrontendEngineData } from "../../../../components/types";
+
+const ERROR_MESSAGE = "test error message";
+
+const renderComponent = (
+	overrideField?: Partial<ITextfieldSchema | IEmailSchema | INumberSchema> | undefined,
+	overrideSchema?: Partial<Omit<IFrontendEngineData, "fields">> | undefined
+) => {
+	const json: IFrontendEngineData = {
+		id: "test",
+		fields: {
+			field: {
+				label: "Textfield",
+				fieldType: "text",
+				...overrideField,
+			},
+			submit: {
+				label: "Submit",
+				fieldType: "submit",
+			},
+		},
+		...overrideSchema,
+	};
+	return render(<FrontendEngine data={json} onSubmit={() => null} />);
+};
+
+describe("textfield", () => {
+	describe("text", () => {
+		it("should be able to render the field", () => {
+			const component = renderComponent();
+			expect(component.container.querySelector(`input[type=text]#field-base`)).toBeInTheDocument();
+		});
+
+		it("should support validation schema", async () => {
+			renderComponent({ validation: [{ required: true, errorMessage: ERROR_MESSAGE }] });
+			await waitFor(() => fireEvent.click(screen.getByTestId("submit")));
+
+			expect(screen.getByText(ERROR_MESSAGE)).toBeInTheDocument();
+		});
+
+		it("should apply inputMode according to its type", () => {
+			const component = renderComponent();
+			expect(component.container.querySelector(`input[inputMode=text]#field-base`)).toBeInTheDocument();
+		});
+
+		it("should support default value", () => {
+			const defaultValue = "hello";
+			renderComponent(undefined, {
+				defaultValues: {
+					field: defaultValue,
+				},
+			});
+
+			expect(screen.getByDisplayValue(defaultValue)).toBeInTheDocument();
+		});
+
+		it("should pass other props into the field", () => {
+			renderComponent({
+				maxLength: 10,
+				placeholder: "placeholder",
+				readOnly: true,
+				disabled: true,
+			});
+			expect(screen.getByTestId("field").getAttribute("maxLength")).toBe("10");
+			expect(screen.getByTestId("field").getAttribute("placeholder")).toBe("placeholder");
+			expect(screen.getByTestId("field").getAttribute("readOnly")).toBeDefined();
+			expect(screen.getByTestId("field").getAttribute("disabled")).toBeDefined();
+		});
+	});
+
+	describe("email", () => {
+		it("should be able to render the field", () => {
+			const component = renderComponent({ fieldType: "email" });
+			expect(component.container.querySelector(`input[type=email]#field-base`)).toBeInTheDocument();
+		});
+
+		it("should validate email format", async () => {
+			renderComponent({ fieldType: "email" });
+			fireEvent.change(screen.getByTestId("field"), { target: { value: "hello" } });
+			await waitFor(() => fireEvent.click(screen.getByTestId("submit")));
+
+			expect(screen.queryByTestId("field-error-message")).toBeInTheDocument();
+		});
+
+		it("should support validation schema", async () => {
+			renderComponent({ fieldType: "email", validation: [{ required: true, errorMessage: ERROR_MESSAGE }] });
+			await waitFor(() => fireEvent.click(screen.getByTestId("submit")));
+
+			expect(screen.getByText(ERROR_MESSAGE)).toBeInTheDocument();
+		});
+
+		it("should apply inputMode according to its type", () => {
+			const component = renderComponent({ fieldType: "email" });
+			expect(component.container.querySelector(`input[inputMode=email]#field-base`)).toBeInTheDocument();
+		});
+
+		it("should support default value", () => {
+			const defaultValue = "john@doe.tld";
+			renderComponent(
+				{ fieldType: "email" },
+				{
+					defaultValues: {
+						field: defaultValue,
+					},
+				}
+			);
+
+			expect(screen.getByDisplayValue(defaultValue)).toBeInTheDocument();
+		});
+
+		it("should pass other props into the field", () => {
+			renderComponent({
+				fieldType: "email",
+				maxLength: 10,
+				placeholder: "placeholder",
+				readOnly: true,
+				disabled: true,
+			});
+			expect(screen.getByTestId("field").getAttribute("maxLength")).toBe("10");
+			expect(screen.getByTestId("field").getAttribute("placeholder")).toBe("placeholder");
+			expect(screen.getByTestId("field").getAttribute("readOnly")).toBeDefined();
+			expect(screen.getByTestId("field").getAttribute("disabled")).toBeDefined();
+		});
+	});
+
+	describe("number", () => {
+		it("should be able to render the field", () => {
+			const component = renderComponent({ fieldType: "number" });
+
+			expect(component.container.querySelector("input[type=number]#field-base")).toBeInTheDocument();
+		});
+
+		it("should support validation schema", async () => {
+			renderComponent({ fieldType: "number", validation: [{ required: true, errorMessage: ERROR_MESSAGE }] });
+			await waitFor(() => fireEvent.click(screen.getByTestId("submit")));
+
+			expect(screen.getByText(ERROR_MESSAGE)).toBeInTheDocument();
+		});
+
+		it("should apply inputMode according to its type", () => {
+			const component = renderComponent({ fieldType: "number" });
+			expect(component.container.querySelector(`input[inputMode=numeric]#field-base`)).toBeInTheDocument();
+		});
+
+		it("should support default value", () => {
+			const defaultValue = 1;
+			renderComponent(
+				{ fieldType: "number" },
+				{
+					defaultValues: {
+						field: defaultValue,
+					},
+				}
+			);
+
+			expect(screen.getByDisplayValue(defaultValue)).toBeInTheDocument();
+		});
+
+		it("should pass other props into the field", () => {
+			renderComponent({
+				fieldType: "number",
+				placeholder: "placeholder",
+				readOnly: true,
+				disabled: true,
+			});
+			expect(screen.getByTestId("field").getAttribute("placeholder")).toBe("placeholder");
+			expect(screen.getByTestId("field").getAttribute("readOnly")).toBeDefined();
+			expect(screen.getByTestId("field").getAttribute("disabled")).toBeDefined();
+		});
+	});
+});
