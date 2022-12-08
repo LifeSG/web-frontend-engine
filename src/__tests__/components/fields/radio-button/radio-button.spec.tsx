6740d59970c65c97a026ff500a0f68aac715e005
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-12-08 15:20:30 +0800

    [TESTS][JL] Create unit tests for radio-button component

---
+++ src/__tests__/components/fields/radio-button/radio-button.spec.tsx
@@ -0,0 +1,66 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { FrontendEngine } from "../../../../components";
+import { IRadioButtonGroupSchema } from "../../../../components/fields";
+import { IFrontendEngineData } from "../../../../components/frontend-engine";
+import { TestHelper } from "../../../../utils";
+import { ERROR_MESSAGE, TOverrideField, TOverrideSchema } from "../../../common";
+
+const submitFn = jest.fn();
+const componentId = "field";
+const fieldType = "radio";
+const componentTestId = TestHelper.generateId(componentId, fieldType);
+
+const renderComponent = (overrideField?: TOverrideField<IRadioButtonGroupSchema>, overrideSchema?: TOverrideSchema) => {
+	const json: IFrontendEngineData = {
+		id: "test",
+		fields: {
+			[componentId]: {
+				label: "Radio",
+				fieldType,
+				options: [
+					{ label: "A", value: "A" },
+					{ label: "B", value: "B" },
+				],
+				...overrideField,
+			},
+			submit: {
+				label: "Submit",
+				fieldType: "submit",
+			},
+		},
+		...overrideSchema,
+	};
+	return render(<FrontendEngine data={json} onSubmit={submitFn} />);
+};
+
+describe(fieldType, () => {
+	it("should be able to render the field", () => {
+		renderComponent();
+		expect(screen.getAllByTestId(componentTestId)).toHaveLength(2);
+	});
+
+	it("should be able to support default values", async () => {
+		const defaultValue = "A";
+		renderComponent(undefined, { defaultValues: { field: defaultValue } });
+
+		await waitFor(() => fireEvent.click(screen.getByTestId("submit")));
+
+		expect(submitFn).toBeCalledWith(expect.objectContaining({ [componentId]: defaultValue }));
+	});
+
+	it("should be able to support validation schema", async () => {
+		renderComponent({ validation: [{ required: true, errorMessage: ERROR_MESSAGE }] });
+
+		await waitFor(() => fireEvent.click(screen.getByTestId("submit")));
+
+		expect(screen.getByText(ERROR_MESSAGE)).toBeInTheDocument();
+	});
+
+	it("should be disabled if configured", async () => {
+		renderComponent({ disabled: true });
+
+		await waitFor(() => fireEvent.click(screen.getByTestId("submit")));
+
+		expect(submitFn).toBeCalledWith(expect.objectContaining({ [componentId]: undefined }));
+	});
+});
