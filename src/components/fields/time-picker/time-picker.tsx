a717c70a273004d5339f12aedf76e2919d1f4b98
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-12-14 15:02:34 +0800

    [TESTS][JL] Refactor unit tests for time-picker

--- src/components/fields/time-picker/time-picker.tsx
+++ src/components/fields/time-picker/time-picker.tsx
@@ -67,6 +67,7 @@ export const TimePicker = (props: IGenericFieldProps<ITimePickerSchema>) => {
 			{...otherSchema}
 			{...otherProps}
 			id={id}
+			aria-label={id}
 			data-testid={TestHelper.generateId(id, "time")}
 			label={label}
 			errorMessage={error?.message}
