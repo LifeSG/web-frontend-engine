055c7a2081b4eeac2ecbca0ef113d47285915fa7
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-12-16 14:52:29 +0800

    [CHIPS][JL] Add aria-pressed to props

--- src/components/shared/chip/chip.tsx
+++ src/components/shared/chip/chip.tsx
@@ -7,7 +7,7 @@ import { IChipButtonProps } from "./types";
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, IChipButtonProps {}
 
 export const Chip = ({ children, ...otherProps }: IProps) => (
-	<ChipButton type="button" {...otherProps}>
+	<ChipButton type="button" aria-pressed={otherProps?.isActive} {...otherProps}>
 		<Text.XSmall weight="semibold">{children}</Text.XSmall>
 	</ChipButton>
 );
