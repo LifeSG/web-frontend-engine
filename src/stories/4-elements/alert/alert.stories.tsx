cd17e1c5ac8aaec8b823d5cae55b25ecb0d1fb9b
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-12-20 15:31:34 +0800

    [ALERT][JL] Create storybook

---
+++ src/stories/4-elements/alert/alert.stories.tsx
@@ -0,0 +1,63 @@
+import { ArgsTable, Description, Heading, PRIMARY_STORY, Stories, Title } from "@storybook/addon-docs";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { FrontendEngine } from "../../../components";
+import { IAlertSchema } from "../../../components/elements";
+import { CommonFieldStoryProps, ExcludeReactFormHookProps } from "../../common";
+
+export default {
+	title: "Element/Alert",
+	parameters: {
+		docs: {
+			page: () => (
+				<>
+					<Title>Alert</Title>
+					<Description>
+						This component renders a `Alert` component provided by the Design System within a Frontend
+						Engine generated form
+					</Description>
+					<Heading>Props</Heading>
+					<Description>
+						Please refer to the [design
+						system](https://designsystem.life.gov.sg/react/index.html?path=/docs/modules-alert--alert) for
+						the properties of `Alert`
+					</Description>
+					<ArgsTable story={PRIMARY_STORY} />
+					<Stories includePrimary={true} title="Example" />
+				</>
+			),
+		},
+	},
+	argTypes: {
+		...ExcludeReactFormHookProps,
+		...CommonFieldStoryProps("alert", true),
+	},
+} as Meta;
+
+const Template: Story<Record<string, IAlertSchema>> = (args) => <FrontendEngine data={{ fields: { ...args } }} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	"alert-default": {
+		fieldType: "alert",
+		type: "success",
+		children: "This is a success message",
+	},
+};
+
+export const ReactNodeChildren = Template.bind({});
+ReactNodeChildren.args = {
+	"alert-react-node": {
+		fieldType: "alert",
+		type: "success",
+		children: (
+			<p>
+				You can add <strong>bold text</strong> to signify or highlight certain information. Or perhaps even add
+				a&nbsp;
+				<a href="https://life.gov.sg" target="_blank" rel="noreferrer">
+					hyperlink
+				</a>
+				&nbsp;to direct users to some external source.
+			</p>
+		),
+	},
+};
