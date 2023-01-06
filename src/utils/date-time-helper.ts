72dda77721c39827d0e6115f86121f78c40ba73c
Wei Li <ng_wei_li@tech.gov.sg>
2023-01-06 16:24:47 +0800

    [MISC][WL] fix circular dependency error with date-time-helper

--- src/utils/date-time-helper.ts
+++ src/utils/date-time-helper.ts
@@ -1,6 +1,6 @@
 import { DateTimeFormatter, LocalDate, LocalDateTime, LocalTime, ResolverStyle } from "@js-joda/core";
 import { Locale } from "@js-joda/locale_en";
-import { ERROR_MESSAGES } from "../components/shared";
+import { ERROR_MESSAGES } from "../components/shared/error-messages"; // import directly to avoid circular dependency
 
 export namespace DateTimeHelper {
 	// TODO: split into individual functions by type when parsing/formatting gets more complicated
