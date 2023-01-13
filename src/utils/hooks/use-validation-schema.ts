debbece73f47ce7fdb33d8f86a821a6899074bbd
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-01-13 15:49:14 +0800

    [VALIDATION][JL] Remove duplicate methods in useValidationSchema hook

--- src/utils/hooks/use-validation-schema.ts
+++ src/utils/hooks/use-validation-schema.ts
@@ -2,13 +2,13 @@ import { useContext, useEffect, useState } from "react";
 import * as Yup from "yup";
 import { ObjectShape } from "yup/lib/object";
 import { TFrontendEngineValues } from "../../components/frontend-engine/types";
-import { IYupValidationRule, YupContext, YupHelper } from "../../components/frontend-engine/yup";
+import { YupContext, YupHelper } from "../../components/frontend-engine/yup";
 
 /**
  * Hook that handles the generation of the validationSchema
  */
 export const useValidationSchema = () => {
-	const { formValidationConfig, setFormValidationConfig } = useContext(YupContext);
+	const { formValidationConfig } = useContext(YupContext);
 	const [hardValidationSchema, setHardValidationSchema] = useState<Yup.ObjectSchema<ObjectShape>>();
 	const [softValidationSchema, setSoftValidationSchema] = useState<Yup.ObjectSchema<ObjectShape>>();
 	const [warnings, setWarnings] = useState<Record<string, string>>({});
@@ -20,35 +20,6 @@ export const useValidationSchema = () => {
 			setSoftValidationSchema(softSchema);
 		}
 	}, [formValidationConfig]);
-
-	/**
-	 * Sets the validation config in the context provider by field id
-	 * @param id field id
-	 * @param schema Yup schema object, add your custom validation logic here
-	 * @param validationRules array validation rules passed from JSON
-	 */
-	const setFieldValidationConfig = <V = IYupValidationRule>(
-		id: string,
-		schema: Yup.AnySchema,
-		validationRules: V[] = [],
-		allowSoftValidation?: boolean
-	) => {
-		setFormValidationConfig((oldConfig) => ({
-			...oldConfig,
-			[id]: { schema, validationRules, allowSoftValidation },
-		}));
-	};
-
-	/**
-	 * Remove field id from validation config to prevent unnecessary validations
-	 * @param id field id
-	 */
-	const removeFieldValidationConfig = (id: string) =>
-		setFormValidationConfig((oldConfig) => {
-			const newConfig = { ...oldConfig };
-			delete newConfig[id];
-			return newConfig;
-		});
 
 	/**
 	 * Executes validation based on allowSoftValidation flag provided in the schema to generate warning messages
@@ -74,9 +45,6 @@ export const useValidationSchema = () => {
 		warnings,
 		softValidationSchema,
 		hardValidationSchema,
-		formValidationConfig,
 		performSoftValidation,
-		setFieldValidationConfig,
-		removeFieldValidationConfig,
 	};
 };
