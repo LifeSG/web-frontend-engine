637a88eea7afdc5c916f37fcd85fbfc9a559f0bb
Wei Li <ng_wei_li@tech.gov.sg>
2023-03-28 10:36:18 +0800

    [JSON][WL] create sections component

---
+++ src/components/elements/sections/sections.tsx
@@ -0,0 +1,32 @@
+import { Section } from "../section";
+import { ISectionsProps } from "./types";
+
+/**
+ * this component is meant to render "pages" which consists of individual Section component
+ * it is not rendering in pages yet because there are no use cases yet
+ * so this is just a placeholder for now
+ *
+ * this component renders the sections object and cannot be defined via `uiType`
+ * i.e. it is used internally only
+ *
+ * TODO:
+ * - render pages properly
+ * - handle validation and errors in each section before navigating away
+ */
+export const Sections = (props: ISectionsProps) => {
+	// =============================================================================
+	// CONST, STATE, REF
+	// =============================================================================
+	const { schema, warnings } = props;
+
+	// =============================================================================
+	// RENDER FUNCTIONS
+	// =============================================================================
+	const renderSections = () => {
+		return Object.entries(schema).reduce((renderList, [id, section], i) => {
+			return [...renderList, <Section key={`section-${i}`} id={id} schema={section} warnings={warnings} />];
+		}, []);
+	};
+
+	return <>{renderSections()}</>;
+};
