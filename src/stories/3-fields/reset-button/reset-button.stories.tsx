85d718c03edbba9593f7178fbdd400c65a9b7ca4
Daniel Sim <daniel_sim@tech.gov.sg>
2023-04-19 18:27:10 +0800

    [RESET-BUTTON] added reset button story

---
+++ src/stories/3-fields/reset-button/reset-button.stories.tsx
@@ -0,0 +1,103 @@
+import { ArgsTable, Description, Heading, PRIMARY_STORY, Stories, Title } from "@storybook/addon-docs";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { IResetButtonSchema } from "../../../components/fields";
+import { CommonFieldStoryProps, FrontendEngine } from "../../common";
+
+export default {
+	title: "Field/ResetButton",
+	parameters: {
+		docs: {
+			page: () => (
+				<>
+					<Title>ResetButton</Title>
+					<Description>The primary call to action component</Description>
+					<Heading>Props</Heading>
+					<Description>
+						This component also inherits the
+						[HTMLButtonElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement)
+						attributes.
+					</Description>
+					<ArgsTable story={PRIMARY_STORY} />
+					<Stories includePrimary={true} title="Examples" />
+				</>
+			),
+		},
+	},
+	argTypes: {
+		...CommonFieldStoryProps("reset"),
+		validation: { table: { disable: true } },
+		styleType: {
+			description: "The style type of the button",
+			table: {
+				type: {
+					summary: "default | secondary | light | link",
+				},
+			},
+			options: ["default", "secondary", "light", "link"],
+			control: {
+				type: "select",
+			},
+		},
+		disabled: {
+			description: "Specifies if the button is interactable",
+			table: {
+				type: {
+					summary: "boolean",
+				},
+				defaultValue: { summary: false },
+			},
+			options: [true, false, "empty-form"],
+			control: {
+				type: "select",
+			},
+		},
+	},
+} as Meta;
+
+const Template = (id: string, defaultValue?: string) =>
+	((args) => (
+		<FrontendEngine
+			data={{
+				sections: {
+					section: {
+						uiType: "section",
+						children: {
+							field: {
+								uiType: "text-field",
+								label: "Field",
+								validation: [{ required: true }],
+							},
+							[id]: args,
+						},
+					},
+				},
+				defaultValues: { field: defaultValue },
+			}}
+		/>
+	)) as Story<IResetButtonSchema>;
+
+export const Default = Template("reset-default").bind({});
+Default.args = {
+	uiType: "reset",
+	label: "Reset",
+};
+
+export const Disabled = Template("reset-disabled").bind({});
+Disabled.args = {
+	uiType: "reset",
+	label: "Reset",
+	disabled: true,
+};
+
+export const Styled = Template("reset-styled").bind({});
+Styled.args = {
+	uiType: "reset",
+	label: "Reset",
+	styleType: "secondary",
+};
+
+export const FieldWithDefaultValue = Template("reset-default-value", "default").bind({});
+FieldWithDefaultValue.args = {
+	uiType: "reset",
+	label: "Reset",
+};
