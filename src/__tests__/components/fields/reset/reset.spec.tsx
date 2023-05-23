0e493f1f61cb8d4bab6fd17a54e913f2486472dc
Yip Jun Han <junhan.y_dcube@scientecpeople.com>
2023-05-23 10:18:32 +0800

    [RESET|IGNORE-DEFAULT][JH] added test case

--- src/__tests__/components/fields/reset/reset.spec.tsx
+++ src/__tests__/components/fields/reset/reset.spec.tsx
@@ -5,8 +5,8 @@ import { IResetButtonSchema } from "../../../../components/fields";
 import { IFrontendEngineData, TFrontendEngineValues } from "../../../../components/types";
 import { FRONTEND_ENGINE_ID, getField, getSubmitButton, getSubmitButtonProps } from "../../../common";
 
-const COMPONENT_ID = "field";
-const COMPONENT_LABEL = "Textfield";
+const TEXT_ID = "field";
+const TEXT_LABEL = "Textfield";
 
 const CHECKBOX_ID = "checkbox";
 const CHECKBOX_LABEL = "Checkbox";
@@ -26,10 +26,9 @@ const renderComponent = (
 			section: {
 				uiType: "section",
 				children: {
-					[COMPONENT_ID]: {
-						label: COMPONENT_LABEL,
+					[TEXT_ID]: {
+						label: TEXT_LABEL,
 						uiType: "text-field",
-						validation: [{ required: true }],
 					},
 					[CHECKBOX_ID]: {
 						label: CHECKBOX_LABEL,
@@ -51,7 +50,7 @@ const renderComponent = (
 };
 
 const getTextfield = (): HTMLElement => {
-	return getField("textbox", COMPONENT_LABEL);
+	return getField("textbox", TEXT_LABEL);
 };
 
 const getResetButton = (): HTMLElement => {
@@ -68,7 +67,7 @@ describe("reset", () => {
 		${"hello"} | ${"is set"}
 		${""}      | ${"is not set"}
 	`("reset the form on press to $description when default value is not set", async ({ result }) => {
-		renderComponent(undefined, _.isEmpty(result) ? undefined : { [COMPONENT_ID]: "hello" });
+		renderComponent(undefined, _.isEmpty(result) ? undefined : { [TEXT_ID]: "hello" });
 
 		const textfield = getTextfield();
 
@@ -80,30 +79,12 @@ describe("reset", () => {
 		expect(textfield).toHaveValue(result);
 	});
 
-	// it.each`
-	// 	defaultValue | expected | type
-	// 	${"hello"}   | ${""}    | ${"string"}
-	// 	${123}       | ${""}    | ${"number"}
-	// 	${["Apple"]} | ${[]}    | ${"array"}
-	// `(
-	// 	"reset the form on press to '' when default value is $type, ignoreDefaultValue:true",
-	// 	async ({ defaultValue, expected }) => {
-	// 		renderComponent({ ignoreDefaultValues: true }, { [COMPONENT_ID]: defaultValue });
-
-	// 		const textfield = getTextfield();
-	// 		const
-
-	// 		await waitFor(() => fireEvent.click(getResetButton()));
-	// 		expect(textfield).toHaveValue(expected);
-	// 	}
-	// );
-
 	it("reset the form on press when default value is set, ignoreDefaultValue:true", async () => {
-		renderComponent({ ignoreDefaultValues: true }, { [CHECKBOX_ID]: ["Apple"], [COMPONENT_ID]: "hello" });
+		renderComponent({ ignoreDefaultValues: true }, { [CHECKBOX_ID]: ["Apple"], [TEXT_ID]: "hello" });
 
 		await waitFor(() => fireEvent.click(getResetButton()));
 		await waitFor(() => fireEvent.click(getSubmitButton()));
-		expect(SUBMIT_FN).toBeCalledWith(expect.objectContaining({ [CHECKBOX_ID]: [], [COMPONENT_ID]: "" }));
+		expect(SUBMIT_FN).toBeCalledWith(expect.objectContaining({ [CHECKBOX_ID]: [], [TEXT_ID]: "" }));
 	});
 
 	it("should be disabled if configured", async () => {
