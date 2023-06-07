a801d8906e31611c38704e76c2b62105fb757bbe
Wei Li <ng_wei_li@tech.gov.sg>
2023-06-07 10:56:44 +0800

    [IMAGE_UPLOAD][WL] add aria-label for delete button

--- src/components/fields/image-upload/image-input/file-item/file-item.tsx
+++ src/components/fields/image-upload/image-input/file-item/file-item.tsx
@@ -121,6 +121,7 @@ export const FileItem = ({ id = "file-item", index, fileItem, maxSizeInKb, accep
 					onClick={onDelete(index)}
 					id={TestHelper.generateId(`${id}-${index + 1}`, "btn-delete")}
 					data-testid={TestHelper.generateId(`${id}-${index + 1}`, "btn-delete")}
+					aria-label={`remove ${fileName}`}
 				>
 					<CrossIcon />
 				</DeleteButton>
