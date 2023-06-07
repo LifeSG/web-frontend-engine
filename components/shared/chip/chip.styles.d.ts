fad9dfcff627d0307727f6598de8257d7c7f6e53
Docker Alpha <bamboo@ip-172-16-237-144.ap-southeast-1.compute.internal>
2023-06-07 16:41:20 +0800

    Built package | Version: v1.0.0-alpha.6 | Bamboo: web-frontend-engine main #49

---
+++ components/shared/chip/chip.styles.d.ts
@@ -0,0 +1,3 @@
+import { IChipButtonProps } from "./types";
+export declare const ChipButton: import("styled-components").StyledComponent<"button", any, IChipButtonProps, never>;
+export declare const ChipText: import("styled-components").StyledComponent<"span", any, import("@lifesg/react-design-system/text").TextProps, never>;
---
+++ components/shared/sanitize/sanitize.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+interface IProps {
+    id?: string;
+    children: string | React.ReactNode;
+}
+export declare const Sanitize: (props: IProps) => import("react/jsx-runtime").JSX.Element;
+export {};
