e5f8d8a3f5e4d304cdb2b290729c05fa393455b7
Wei Li <ng_wei_li@tech.gov.sg>
2023-07-06 15:04:27 +0800

    [OVERRIDES][WL] refactor rendering of various wrapper children

--- src/components/elements/wrapper/wrapper.tsx
+++ src/components/elements/wrapper/wrapper.tsx
@@ -1,119 +1,25 @@
 import isEmpty from "lodash/isEmpty";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, ReactNode, useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
+import useDeepCompareEffect from "use-deep-compare-effect";
 import * as FrontendEngineElements from "..";
-import * as FrontendEngineCustomComponents from "../../custom";
 import { TestHelper } from "../../../utils";
+import * as FrontendEngineCustomComponents from "../../custom";
 import * as FrontendEngineFields from "../../fields";
 import {
 	ECustomElementType,
 	ECustomFieldType,
 	EElementType,
 	EFieldType,
-	IGenericFieldProps,
 	TFrontendEngineFieldSchema,
 } from "../../frontend-engine/types";
 import { ERROR_MESSAGES } from "../../shared";
 import { ConditionalRenderer } from "./conditional-renderer";
 import { IWrapperProps } from "./types";
 import { DSAlert } from "./wrapper.styles";
-import { IFilterItemSchema } from "../../custom/filter/filter-item/types";
-import { IFilterCheckboxSchema } from "../../custom/filter/filter-checkbox/types";
 
 const fieldTypeKeys = Object.keys(EFieldType);
 const elementTypeKeys = Object.keys(EElementType);
-type TComponentRenderType = {
-	fieldType?: React.ElementType | undefined;
-	elementType?: React.ElementType | undefined;
-	fragment?: React.ReactElement | undefined;
-};
-
-const renderField = (
-	Field: React.ElementType,
-	id: string,
-	schema: TFrontendEngineFieldSchema,
-	control,
-	warnings: Record<string, string>
-) => {
-	return (
-		<ConditionalRenderer
-			id={id}
-			key={id}
-			{...(schema && "showIf" in schema && { renderRules: schema.showIf })}
-			schema={schema}
-		>
-			<Controller
-				control={control}
-				name={id}
-				shouldUnregister={true}
-				render={({ field, fieldState }) => {
-					const fieldProps = { ...field, id, ref: undefined }; // not passing ref because not all components have fields to be manipulated
-					const warning = warnings?.[id];
-
-					if (!warning) {
-						return <Field schema={schema} {...fieldProps} {...fieldState} />;
-					}
-					return (
-						<>
-							<Field schema={schema} {...fieldProps} {...fieldState} />
-							<DSAlert type="warning">{warning}</DSAlert>
-						</>
-					);
-				}}
-			/>
-		</ConditionalRenderer>
-	);
-};
-
-const renderElement = (
-	Element: React.ElementType,
-	id: string,
-	schema: TFrontendEngineFieldSchema | IFilterCheckboxSchema | IFilterItemSchema
-) => {
-	return (
-		<ConditionalRenderer
-			id={id}
-			key={id}
-			{...(schema && "showIf" in schema && { renderRules: schema.showIf })}
-			schema={schema}
-		>
-			<Element schema={schema} id={id} />
-		</ConditionalRenderer>
-	);
-};
-
-const getCustomComponentType = (referenceKey: string) => {
-	const refKey = referenceKey?.toUpperCase();
-	// Should check if CustomElement
-	if (FrontendEngineCustomComponents[ECustomFieldType[refKey]]) {
-		const fieldType = FrontendEngineCustomComponents[ECustomFieldType[refKey]];
-		return { fieldType };
-	} else if (FrontendEngineCustomComponents[ECustomElementType[refKey]]) {
-		const elementType = FrontendEngineCustomComponents[ECustomElementType[refKey]];
-		return { elementType };
-	}
-};
-
-const getComponentTypeOrFragment = (uiType: string, id: string) => {
-	// TODO: Refactor to fucntion
-	const UIType = uiType?.toUpperCase();
-	const frontendEngineComponents = { ...FrontendEngineFields, ...FrontendEngineElements };
-
-	if (fieldTypeKeys.includes(UIType)) {
-		// render fields with controller to register them into react-hook-form
-		const fieldType = frontendEngineComponents[EFieldType[UIType]];
-		return { fieldType };
-	} else if (elementTypeKeys.includes(UIType)) {
-		// render other elements as normal components
-		const elementType = (frontendEngineComponents[EElementType[UIType]] ||
-			Wrapper) as React.ForwardRefExoticComponent<IGenericFieldProps<TFrontendEngineFieldSchema>>;
-		return { elementType };
-	} else {
-		// need uiType check to ignore other storybook args
-		const fragment = <Fragment key={id}>{ERROR_MESSAGES.GENERIC.UNSUPPORTED}</Fragment>;
-		return { fragment };
-	}
-};
 
 export const Wrapper = (props: IWrapperProps): JSX.Element | null => {
 	// =============================================================================
@@ -133,24 +39,35 @@ export const Wrapper = (props: IWrapperProps): JSX.Element | null => {
 	 * - render strings directly
 	 * - otherwise show field not supported error
 	 */
-	useEffect(() => {
+	useDeepCompareEffect(() => {
 		const wrapperChildren = schemaChildren || children;
 		if (typeof wrapperChildren === "object") {
 			const renderComponents: JSX.Element[] = [];
 
-			Object.entries(wrapperChildren).forEach(([id, child]) => {
-				if (isEmpty(child) || typeof child !== "object") return;
-				const compType: TComponentRenderType = child.referenceKey
-					? getCustomComponentType(child.referenceKey)
-					: getComponentTypeOrFragment(child.uiType, id);
-				if (!compType) return;
-				if (compType.fragment) {
-					renderComponents.push(compType.fragment);
-				} else {
-					compType.fieldType
-						? renderComponents.push(renderField(compType.fieldType, id, child, control, warnings))
-						: renderComponents.push(renderElement(compType.elementType, id, child));
+			Object.keys(wrapperChildren).forEach((childId) => {
+				const childSchema = wrapperChildren[childId];
+				if (isEmpty(childSchema) || typeof childSchema !== "object") return;
+				let renderComponent: ReactNode = (
+					<Fragment key={childId}>{ERROR_MESSAGES.GENERIC.UNSUPPORTED}</Fragment>
+				);
+				if ("referenceKey" in childSchema) {
+					const referenceKey = childSchema.referenceKey.toUpperCase();
+					if (FrontendEngineCustomComponents[ECustomFieldType[referenceKey]]) {
+						renderComponent = buildConditionalRenderer(childId, childSchema)(buildField);
+					} else if (FrontendEngineCustomComponents[ECustomElementType[referenceKey]]) {
+						renderComponent = buildConditionalRenderer(childId, childSchema)(buildElement);
+					} else {
+						// let custom components fail silently
+						renderComponent = <></>;
+					}
+				} else if ("uiType" in childSchema) {
+					if (fieldTypeKeys.includes(childSchema.uiType.toUpperCase())) {
+						renderComponent = buildConditionalRenderer(childId, childSchema)(buildField);
+					} else if (elementTypeKeys.includes(childSchema.uiType.toUpperCase())) {
+						renderComponent = buildConditionalRenderer(childId, childSchema)(buildElement);
+					}
 				}
+				renderComponents.push(renderComponent);
 			});
 			setComponents(renderComponents);
 		} else if (typeof wrapperChildren === "string") {
@@ -160,6 +77,61 @@ export const Wrapper = (props: IWrapperProps): JSX.Element | null => {
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [schemaChildren || children, control, warnings]);
+
+	// =============================================================================
+	// HELPER FUNCTIONS
+	// =============================================================================
+	const buildConditionalRenderer =
+		(childId: string, childSchema: TFrontendEngineFieldSchema) =>
+		(buildFn: (childId: string, child: TFrontendEngineFieldSchema) => JSX.Element) =>
+			(
+				<ConditionalRenderer
+					id={childId}
+					key={childId}
+					{...(childSchema && "showIf" in childSchema && { renderRules: childSchema.showIf })}
+					schema={childSchema}
+				>
+					{buildFn(childId, childSchema)}
+				</ConditionalRenderer>
+			);
+
+	const buildField = (childId: string, childSchema: TFrontendEngineFieldSchema) => {
+		let Field;
+		if ("uiType" in childSchema) {
+			Field = FrontendEngineFields[EFieldType[childSchema.uiType?.toUpperCase()]];
+		} else if ("referenceKey" in childSchema) {
+			Field = FrontendEngineCustomComponents[ECustomFieldType[childSchema.referenceKey?.toUpperCase()]];
+		}
+
+		if (!Field) return null;
+		return (
+			<Controller
+				control={control}
+				name={childId}
+				shouldUnregister={true}
+				render={({ field, fieldState }) => {
+					const fieldProps = { ...field, id: childId, ref: undefined }; // not passing ref because not all components have fields to be manipulated
+					const warning = warnings?.[childId];
+					return (
+						<>
+							<Field schema={childSchema} {...fieldProps} {...fieldState} />
+							{warning && <DSAlert type="warning">{warning}</DSAlert>}
+						</>
+					);
+				}}
+			/>
+		);
+	};
+
+	const buildElement = (childId: string, childSchema: TFrontendEngineFieldSchema) => {
+		let Element;
+		if ("uiType" in childSchema) {
+			Element = FrontendEngineElements[EElementType[childSchema.uiType?.toUpperCase()]] || Wrapper;
+		} else if ("referenceKey" in childSchema) {
+			Element = FrontendEngineCustomComponents[ECustomElementType[childSchema.referenceKey?.toUpperCase()]];
+		}
+		return <Element schema={childSchema} id={childId} />;
+	};
 
 	// =============================================================================
 	// RENDER FUNCTIONS
