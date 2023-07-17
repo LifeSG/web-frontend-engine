5513ddf465da902a3621c2ed6b393091c4dddcfd
Yihui <see_yihui@tech.gov.sg>
2023-07-17 10:50:59 +0800

    [MOL-13278][YH] Update comments for getCurrentLocation

--- src/utils/geolocation-helper.ts
+++ src/utils/geolocation-helper.ts
@@ -22,7 +22,7 @@ export namespace GeoLocationHelper {
 	};
 
 	/**
-	 * @param options An optional options object that lets you specify the maximum no. of attempts (default: 1),
+	 * @param options An optional options object that lets you specify the maximum no. of attempts (default: 3),
 	 * the timeout in milliseconds (default: 2000), the maximum age of a possible cached position in milliseconds (default: 2000)
 	 * A minimum of one attempt will be made, even if maxAttempts is 0.
 	 * @returns A Promise that gets the current location.
