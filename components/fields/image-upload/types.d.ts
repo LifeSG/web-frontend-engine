47aebd4976f17961b16b0241018a39751673233f
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2023-07-19 11:13:00 +0800

    Built package | Version: v1.0.0-alpha.11 | Gitlab: web-frontend-engine main 1359970

---
+++ components/fields/image-upload/types.d.ts
@@ -0,0 +1,65 @@
+import { IFrontendEngineBaseFieldJsonSchema } from "../../frontend-engine";
+import { IYupValidationRule } from "../../frontend-engine/yup/types";
+export type TUploadMethod = "post" | "put" | "patch";
+export declare const ACCEPTED_FILE_TYPES: readonly ["jpg", "gif", "png", "heic", "heif", "webp"];
+export type TImageUploadAcceptedFileType = (typeof ACCEPTED_FILE_TYPES)[number];
+export type TImageUploadOutputFileType = "jpg" | "png";
+export interface IImageUploadValidationRule extends IYupValidationRule {
+    /** accepted file types */
+    fileType?: TImageUploadAcceptedFileType[] | undefined;
+    /** max acceptable file size in kb */
+    maxSizeInKb?: number | undefined;
+}
+export interface IImageUploadSchema<V = undefined> extends IFrontendEngineBaseFieldJsonSchema<"image-upload", V, IImageUploadValidationRule> {
+    buttonLabel?: string | undefined;
+    description?: string | undefined;
+    editImage?: boolean | undefined;
+    outputType?: TImageUploadOutputFileType | undefined;
+    uploadOnAddingFile?: {
+        method: TUploadMethod;
+        url: string;
+    } | undefined;
+    compress?: boolean | undefined;
+    dimensions?: IImageDimensions | undefined;
+}
+export interface ISharedImageProps {
+    id?: string | undefined;
+    accepts: TImageUploadAcceptedFileType[];
+    maxSizeInKb: number;
+    maxFiles: number;
+}
+export declare enum EImageStatus {
+    INJECTED = -99,
+    ERROR_FORMAT = -3,
+    ERROR_GENERIC = -2,
+    ERROR_SIZE = -1,
+    NONE = 0,
+    TO_RECOMPRESS = 1,
+    COMPRESSED = 2,
+    RECOMPRESSED = 2,
+    CONVERTED = 2,
+    EDITED = 3,
+    UPLOAD_READY = 4,
+    UPLOADING = 5,
+    UPLOADED = 6
+}
+export interface IImage {
+    file: File;
+    name: string;
+    type?: string;
+    /** refers to preview dimensions and eventual output dimensions */
+    dimensions: IImageDimensions;
+    dataURL?: string;
+    thumbnailDataURL?: string;
+    drawingDataURL?: string;
+    drawing?: fabric.Object[];
+    status: EImageStatus;
+    addedFrom?: "dragInput" | "reviewModal" | "schema";
+    uploadProgress: number;
+    uploadResponse?: any;
+    slot: number;
+}
+export interface IImageDimensions {
+    width: number;
+    height: number;
+}
