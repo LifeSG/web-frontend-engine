a5f49cc2e2b51c5c0ee6878349f16a25565cbbd6
Quek Ruo Ling <ruoling.quek@ufinity.com>
2023-08-03 14:25:09 +0800

    [CONDREND][RL] Create stories

---
+++ src/stories/2-frontend-engine/conditional-rendering/conditional-rendering-restore-mode.stories.tsx
@@ -0,0 +1,110 @@
+import { Button } from "@lifesg/react-design-system/button";
+import { Description, Stories, Title } from "@storybook/addon-docs";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { useRef } from "react";
+import { IFrontendEngineData, IFrontendEngineProps, IFrontendEngineRef } from "../../../components/frontend-engine";
+import { FrontendEngine, SUBMIT_BUTTON_SCHEMA } from "../../common";
+
+const DATA: IFrontendEngineData = {
+	sections: {
+		section: {
+			uiType: "section",
+			children: {
+				field1: {
+					label: "Field 1",
+					uiType: "text-field",
+					placeholder: 'Type "show"',
+					validation: [{ required: true }],
+				},
+				field2: {
+					label: "Field 2 (depends on Field 1)",
+					uiType: "date-field",
+					showIf: [{ field1: [{ equals: "show" }] }],
+					validation: [{ required: true }],
+					withButton: false,
+				},
+				field3: {
+					label: "Field 3 (depends on Field 1)",
+					uiType: "chips",
+					showIf: [{ field1: [{ equals: "show" }] }],
+					validation: [{ required: true }],
+					options: [
+						{ value: "1", label: "Opt 1" },
+						{ value: "2", label: "Opt 2" },
+						{ value: "3", label: "Opt 3" },
+					],
+					textarea: {
+						label: "Others",
+					},
+				},
+				unrelated: {
+					label: "Unrelated field",
+					uiType: "text-field",
+				},
+				unrelatedWithDefault: {
+					label: "Unrelated field with default",
+					uiType: "text-field",
+				},
+				...SUBMIT_BUTTON_SCHEMA,
+			},
+		},
+	},
+	defaultValues: {
+		field1: "show",
+		field2: "2023-01-01",
+		"field3-textarea": "hello",
+		unrelatedWithDefault: "default",
+	},
+};
+
+export default {
+	title: "Form/Conditional Rendering/Restore",
+	component: null,
+	parameters: {
+		docs: {
+			page: () => (
+				<>
+					<Title>Restore conditionally rendered fields</Title>
+					<Description>
+						You can configure if the current value of a field is persisted when its visibility is toggled.
+					</Description>
+					<Stories includePrimary={true} title="Examples" />
+				</>
+			),
+		},
+	},
+} as Meta;
+
+const Template: Story<IFrontendEngineProps> = (args) => {
+	const ref = useRef<IFrontendEngineRef>();
+	const handleReset = () => {
+		ref.current.reset();
+	};
+
+	return (
+		<>
+			<style dangerouslySetInnerHTML={{ __html: ".hidden {display: none};" }} />
+			<FrontendEngine ref={ref} {...args} />
+			<br />
+			<Button.Default styleType="secondary" onClick={handleReset}>
+				Reset form
+			</Button.Default>
+		</>
+	);
+};
+
+export const RestoreDefaultValue = Template.bind({});
+RestoreDefaultValue.args = {
+	data: {
+		...DATA,
+		restoreMode: "default-value",
+	},
+};
+
+export const RestoreUserInput = Template.bind({});
+RestoreUserInput.args = {
+	data: {
+		...DATA,
+		restoreMode: "user-input",
+	},
+};
