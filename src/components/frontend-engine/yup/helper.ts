5c79bf88af5f0993c2e06c15298437db413930a1
Wei Li <ng_wei_li@tech.gov.sg>
2023-10-17 20:14:14 +0800

    [VALIDATION][WL] skip non-required validation for empty fields

--- src/components/frontend-engine/yup/helper.ts
+++ src/components/frontend-engine/yup/helper.ts
@@ -198,6 +198,13 @@ export namespace YupHelper {
 			if (customRuleKey) {
 				yupSchema = (yupSchema as unknown)[customRuleKey](rule[customRuleKey], rule.errorMessage);
 			}
+
+			// prevent applying non-required validation for empty fields
+			yupSchema = yupSchema.transform((value) => {
+				if (value === null || value === "" || (typeof value === "object" && Object.keys(value).length === 0))
+					return undefined;
+				return value;
+			});
 		});
 
 		return yupSchema;
