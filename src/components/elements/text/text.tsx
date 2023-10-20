5cf5212201cdcbda744d0d8bdc4242edd572e06e
wang_xiang <xiang.wang@ufinity.com>
2023-10-20 20:21:28 +0800

    [CCUBE-382][WX] enhance Text component to be collapsible with view more button

--- src/components/elements/text/text.tsx
+++ src/components/elements/text/text.tsx
@@ -5,6 +5,8 @@ import { Sanitize } from "../../shared";
 import { IGenericElementProps } from "../types";
 import { TEXT_MAPPING } from "./data";
 import { ITextSchema } from "./types";
+import { useEffect, useRef, useState } from "react";
+import { Button } from "@lifesg/react-design-system";
 
 export const Text = (props: IGenericElementProps<ITextSchema>) => {
 	// =============================================================================
@@ -12,10 +14,35 @@ export const Text = (props: IGenericElementProps<ITextSchema>) => {
 	// =============================================================================
 	const {
 		id,
-		schema: { children, uiType, ...otherSchema },
+		schema: { children, uiType, maxLines, ...otherSchema },
 	} = props;
 
+	const contentDivRef = useRef(null);
+	const [isMore, setIsMore] = useState<boolean>(false);
+	const [hideButton, setHideButton] = useState<boolean>(true);
+
 	const Element = TEXT_MAPPING[uiType.toUpperCase()] || undefined;
+
+	// =============================================================================
+	// EFFECTS / CALLBACKS
+	// =============================================================================
+	useEffect(() => {
+		// isMore: true, text expanded, and view less button display, false: text collaspible, and view more button display
+		setIsMore(!maxLines);
+		// hide the view more/less button
+		setHideButton(!maxLines);
+
+		if (!maxLines || !contentDivRef.current) return;
+
+		// calculate whether to show more after line-clamp is triggered
+		const clientHeight = contentDivRef.current.clientHeight;
+		const scrollHeight = contentDivRef.current.scrollHeight;
+
+		// Note: the calculated scrollHeight and clientHeight differs by 1 without any overflow
+		// setHideButton(!(scrollHeight - clientHeight > 1));
+		setHideButton(!(scrollHeight - clientHeight > 1));
+	}, [children, maxLines]);
+
 	// =============================================================================
 	// HELPER FUNCTIONS
 	// =============================================================================
@@ -52,14 +79,26 @@ export const Text = (props: IGenericElementProps<ITextSchema>) => {
 	};
 
 	return (
-		<Element
-			id={id}
-			data-testid={getTestId(id)}
-			{...otherSchema}
-			// NOTE: Parent text body should be transformed into <div> to prevent validateDOMNesting error
-			{...(hasNestedFields() && { as: "div" })}
-		>
-			<Sanitize id={id}>{renderText()}</Sanitize>
-		</Element>
+		<>
+			<Element
+				id={id}
+				ref={contentDivRef}
+				maxLines={maxLines > 0 && !isMore ? maxLines : undefined}
+				data-testid={getTestId(id)}
+				{...otherSchema}
+				// NOTE: Parent text body should be transformed into <div> to prevent validateDOMNesting error
+				{...(hasNestedFields() && { as: "div" })}
+			>
+				<Sanitize id={id}>{renderText()}</Sanitize>
+			</Element>
+
+			<div>
+				{!hideButton && maxLines > 0 && (
+					<Button.Small styleType="link" onClick={() => setIsMore(!isMore)}>
+						{isMore ? "View less" : "View more"}
+					</Button.Small>
+				)}
+			</div>
+		</>
 	);
 };
