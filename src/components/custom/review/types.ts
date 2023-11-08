2e9c879fd024fa61e282834c58bc3f65249df708
dave <daiproxomhoa@gmail.com>
2023-11-08 10:54:07 +0700

    [CCUBE-546][DV] update code based on comments

--- src/components/custom/review/types.ts
+++ src/components/custom/review/types.ts
@@ -1,26 +1,27 @@
+import { BoxContainerProps } from "@lifesg/react-design-system";
 import { UneditableSectionItemProps } from "@lifesg/react-design-system/uneditable-section/types";
 import type { IAlertSchema, ITextSchema } from "../../elements";
 import type { IWrapperSchema } from "../../elements/wrapper";
 import type { ICustomElementJsonSchema } from "../types";
-import { BoxContainerProps } from "@lifesg/react-design-system";
 
 type TReviewSectionChildren = IAlertSchema | ITextSchema | IWrapperSchema;
 
 export type IReviewSchema = IReviewSchemaAccordion | IReviewSchemaDefault;
 export interface IReviewBase extends ICustomElementJsonSchema<"review"> {
 	items: UneditableSectionItemProps[];
+}
+export interface IReviewSchemaDefault extends IReviewBase {
+	label?: string | undefined;
+	variant?: "default" | undefined;
+	description?: string | undefined;
 	topSection?: Record<string, TReviewSectionChildren> | undefined;
 	bottomSection?: Record<string, TReviewSectionChildren> | undefined;
 }
-export interface IReviewSchemaDefault extends IReviewBase {
-	variant?: "default" | undefined;
-	label?: string | undefined;
-	description?: string | undefined;
-}
 export interface IReviewSchemaAccordion
 	extends IReviewBase,
-		Omit<BoxContainerProps, "children" | "callToActionComponent"> {
-	variant: "accodion";
+		Omit<BoxContainerProps, "children" | "title" | "callToActionComponent" | "subComponentTestIds"> {
+	label: string;
+	variant: "accordion";
 	button?: IButtonAccordion | undefined;
 }
 export interface IButtonAccordion {
