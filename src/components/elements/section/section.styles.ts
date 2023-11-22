172c58953a404ece23204274deb50b97c8361e3c
Wei Li <ng_wei_li@tech.gov.sg>
2023-11-22 17:26:47 +0800

    [SECTION][WL] update layoutType - fix grid padding - use Layout.Content to wrap grid layoutType - add default layoutType - add contain layoutType

---
+++ src/components/elements/section/section.styles.ts
@@ -0,0 +1,10 @@
+import { Layout } from "@lifesg/react-design-system/layout";
+import styled from "styled-components";
+
+export const GridWrapper = styled(Layout.Container)`
+	gap: 2rem;
+`;
+
+export const Contained = styled.div`
+	flex: 1;
+`;
