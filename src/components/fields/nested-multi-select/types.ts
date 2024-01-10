526ffe82d7a2e32ea13f400cad95d7582c716dfc
Daniel Sim <daniel_sim@tech.gov.sg>
2024-01-10 15:53:42 +0800

    [RBS-1084][DS] minor correction to typing

--- src/components/fields/nested-multi-select/types.ts
+++ src/components/fields/nested-multi-select/types.ts
@@ -1,12 +1,20 @@
 import { InputNestedMultiSelectProps } from "@lifesg/react-design-system/input-nested-multi-select";
 import { TComponentOmitProps } from "../../frontend-engine";
 import { IBaseFieldSchema } from "../types";
-import { L1OptionProps, L2OptionProps, L3OptionProps } from "@lifesg/react-design-system";
 
-export type TL1OptionProps = L1OptionProps<string, string, string>;
-export type TL2OptionProps = L2OptionProps<string, string>;
-export type TL3OptionProps = L3OptionProps<string>;
-export type TNestedValues = IL1Value | IL2Value | IL3Value;
+export interface IBaseOptionProps {
+	label: string;
+	key: string;
+}
+interface IBaseOptionPropsWithValues extends IBaseOptionProps {
+	value: string;
+	subItems?: never;
+}
+
+interface IBaseOptionPropsWithSubItems<T> extends IBaseOptionProps {
+	value?: never;
+	subItems: T;
+}
 export interface IL1Value {
 	[key: string]: IL2Value | string;
 }
@@ -16,6 +24,12 @@ export interface IL2Value {
 export interface IL3Value {
 	[key: string]: string;
 }
+
+export type TL1OptionProps = IBaseOptionPropsWithValues | IBaseOptionPropsWithSubItems<TL2OptionProps[]>;
+export type TL2OptionProps = IBaseOptionPropsWithValues | IBaseOptionPropsWithSubItems<TL3OptionProps[]>;
+export type TL3OptionProps = IBaseOptionPropsWithValues;
+
+export type TNestedValues = IL1Value | IL2Value | IL3Value;
 
 export type NestedMultiSelectProps = Pick<
 	InputNestedMultiSelectProps<string, string, string>,
