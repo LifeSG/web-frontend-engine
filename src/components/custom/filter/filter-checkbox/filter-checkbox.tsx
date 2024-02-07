26b9c03cccf0fdbbbd52a74bc52ac73a1cd11227
roll <quek.ruoling@gmail.com>
2024-02-07 22:13:51 +0800

    [CCUBE-892][RL] Add html support to filter checkbox option label

--- src/components/custom/filter/filter-checkbox/filter-checkbox.tsx
+++ src/components/custom/filter/filter-checkbox/filter-checkbox.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import useDeepCompareEffect from "use-deep-compare-effect";
 import { TestHelper } from "../../../../utils";
+import { Sanitize } from "../../../shared";
 import { IGenericCustomFieldProps } from "../../types";
 import { IFilterCheckboxSchema, IOption } from "./types";
 
@@ -56,6 +57,7 @@ export const FilterCheckbox = (props: IGenericCustomFieldProps<IFilterCheckboxSc
 			expanded={expandedState}
 			onExpandChange={setExpandedState}
 			onSelect={handleChange}
+			labelExtractor={(item) => <Sanitize sanitizeOptions={{ allowedAttributes: false }}>{item.label}</Sanitize>}
 		></Filter.Checkbox>
 	);
 };
