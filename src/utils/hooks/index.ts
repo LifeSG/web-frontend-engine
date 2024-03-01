d087d239b4d43dd66a50b6df55762a732c1c64bb
Wei Li <ng_wei_li@tech.gov.sg>
2024-03-01 16:13:40 +0800

    [CUSTOM_COMPONENTS][WL] create useFrontendEngineComponent hook

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -2,6 +2,7 @@ export * from "./use-deep-layout-effect";
 export * from "./use-field-event";
 export * from "./use-form-schema";
 export * from "./use-form-values";
+export * from "./use-frontend-engine-component";
 export * from "./use-previous";
 export * from "./use-validation-config";
 export * from "./use-validation-schema";
