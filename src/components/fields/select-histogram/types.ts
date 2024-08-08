39902a78a2854aec00758d7fbc2cb62c2ef780d9
wang_xiang <xiang.wang@ufinity.com>
2024-08-08 11:58:27 +0800

    [BOOKINGSG-5979][WX]allow customisation of select-histogram error messages

--- src/components/fields/select-histogram/types.ts
+++ src/components/fields/select-histogram/types.ts
@@ -1,11 +1,12 @@
 import { HistogramSliderProps } from "@lifesg/react-design-system/histogram-slider";
 import { TComponentOmitProps } from "../../frontend-engine";
+import { IHistogramValidationRule } from "../histogram-slider";
 import { IBaseFieldSchema } from "../types";
 
 type TCustomOptions = Pick<HistogramSliderProps, "rangeLabelPrefix" | "rangeLabelSuffix">;
 
 export interface ISelectHistogramSchema<V = undefined>
-	extends IBaseFieldSchema<"select-histogram", V>,
+	extends IBaseFieldSchema<"select-histogram", V, IHistogramValidationRule>,
 		TComponentOmitProps<Pick<HistogramSliderProps, "className" | "disabled" | "readOnly" | "bins" | "interval">> {
 	customOptions?: TCustomOptions | undefined;
 }
