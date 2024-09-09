d677b6c414e3c36bc676f317c946b7e0b5f09e9a
Quek Ruo Ling <ruoling.quek@ufinity.com>
2024-09-09 17:45:12 +0800

    [MISC][RL] Restrict children to elements

--- src/components/elements/alert/types.ts
+++ src/components/elements/alert/types.ts
@@ -1,10 +1,10 @@
 import { AlertProps, AlertType } from "@lifesg/react-design-system";
-import { TComponentOmitProps, TFrontendEngineFieldSchema } from "../../frontend-engine";
-import { IBaseElementSchema } from "../types";
+import { TComponentOmitProps } from "../../frontend-engine";
+import { IBaseElementSchema, TElementSchema } from "../types";
 
 export interface IAlertSchema<V = undefined, C = undefined>
 	extends IBaseElementSchema<"alert">,
 		TComponentOmitProps<AlertProps> {
 	type: AlertType;
-	children: React.ReactNode | Record<string, TFrontendEngineFieldSchema<V, C>>;
+	children: React.ReactNode | Record<string, TElementSchema<V, C>>; // use element schema instead
 }
