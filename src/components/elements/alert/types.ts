7f9b33b67778e0e1f0f53ea9be4f229038d492ef
Quek Ruo Ling <ruoling.quek@ufinity.com>
2024-09-10 09:05:30 +0800

    [MISC][RL] Further restrict allowed alert children

--- src/components/elements/alert/types.ts
+++ src/components/elements/alert/types.ts
@@ -1,10 +1,15 @@
 import { AlertProps, AlertType } from "@lifesg/react-design-system";
 import { TComponentOmitProps } from "../../frontend-engine";
-import { IBaseElementSchema, TElementSchema } from "../types";
+import type { IDividerSchema } from "../divider";
+import type { IOrderedListSchema, IUnorderedListSchema } from "../list";
+import type { ITextSchema } from "../text";
+import { IBaseElementSchema } from "../types";
 
 export interface IAlertSchema<V = undefined, C = undefined>
 	extends IBaseElementSchema<"alert">,
 		TComponentOmitProps<AlertProps> {
 	type: AlertType;
-	children: React.ReactNode | Record<string, TElementSchema<V, C>>; // use element schema instead
+	children:
+		| React.ReactNode
+		| Record<string, IDividerSchema | IOrderedListSchema<V, C> | ITextSchema | IUnorderedListSchema<V, C>>;
 }
