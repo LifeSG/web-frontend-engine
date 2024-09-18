6e754eb368b0aca678e2ef65c09aeb5464ea55d2
Wei Li <ng_wei_li@tech.gov.sg>
2024-09-18 23:26:13 +0800

    [NUMERIC][WL] store value as number in local state

--- src/components/fields/text-field/text-field.tsx
+++ src/components/fields/text-field/text-field.tsx
@@ -112,8 +112,13 @@ export const TextField = (props: IGenericFieldProps<ITextFieldSchema | IEmailFie
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
 		if (uiType === "numeric-field") {
 			const isNumber = !isNaN(parseFloat(event.target.value));
-			onChange({ target: { value: isNumber ? +event.target.value : undefined } });
-		} else if (schema.uiType === "text-field") {
+			const valueToUpdate = isNumber ? +event.target.value : undefined;
+			onChange({ target: { value: valueToUpdate } });
+			setStateValue(valueToUpdate);
+			return;
+		}
+
+		if (schema.uiType === "text-field") {
 			caret.current = event.target.selectionEnd; // must save current caret position before mutating event.target
 
 			if (schema.customOptions?.textTransform === "uppercase") {
