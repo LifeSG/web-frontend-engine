225ae90deb95d1d3c81e0d77355b38610775f451
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2024-09-19 23:54:12 +0800

    Built package | Version: v1.17.0 | Gitlab: web-frontend-engine main 6161956

---
+++ components/custom/filter/filter-checkbox/filter-checkbox.d.ts
@@ -0,0 +1,3 @@
+import { IGenericCustomFieldProps } from "../../types";
+import { IFilterCheckboxSchema } from "./types";
+export declare const FilterCheckbox: (props: IGenericCustomFieldProps<IFilterCheckboxSchema>) => import("react/jsx-runtime").JSX.Element;
---
+++ components/frontend-engine/frontend-engine.d.ts
@@ -0,0 +1,23 @@
+import { ReactElement, Ref } from "react";
+import { IYupValidationRule } from "../../context-providers";
+import { TNoInfer } from "../../utils";
+import { IFrontendEngineProps, IFrontendEngineRef } from "./types";
+/**
+ * The one and only component needed to create your form
+ *
+ * Minimally you will need to set the `data` props which is the JSON schema to define the form
+ *
+ * Generics
+ * - V = custom validation types
+ * - C = custom component types
+ */
+export declare const FrontendEngine: <V = undefined, C = undefined>(props: IFrontendEngineProps<TNoInfer<V, IYupValidationRule<undefined, undefined>>, C> & {
+    ref?: Ref<IFrontendEngineRef>;
+}) => ReactElement;
+/**
+ * casting as a function with generics as a way to define generics with forwardRef
+ * the generics are a way to set custom definition of Yup validation config
+ * `NoInfer` typing is to ensure validation config is either the default `IYupValidationRule` or explicitly typed
+ * this prevent inferring from the data props entered by devs
+ * can refer to `Add Custom Validation` story for more info
+ */
