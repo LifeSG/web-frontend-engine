e83eb910a45f917afcabbbfcd8b1ff4856949542
Wei Li <ng_wei_li@tech.gov.sg>
2025-01-17 17:34:35 +0800

    [IFRAME][WL] update iframePostMessage - restrict typing - apply useCallback - add as dependency

--- src/components/custom/iframe/iframe.tsx
+++ src/components/custom/iframe/iframe.tsx
@@ -1,10 +1,20 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import { useFormContext } from "react-hook-form";
+import { FieldError, useFormContext } from "react-hook-form";
 import styled from "styled-components";
 import * as Yup from "yup";
 import { useIframeMessage, useValidationConfig } from "../../../utils/hooks";
 import { IGenericCustomFieldProps } from "../types";
 import { EPostMessageEvent, IIframeSchema } from "./types";
+
+type TIframePostMessageOptions =
+	| {
+			type: EPostMessageEvent.SYNC;
+			payload: { error: FieldError; id: string; value: unknown };
+	  }
+	| {
+			type: EPostMessageEvent.VALIDATE;
+			payload: { value: unknown; isSubmit: boolean };
+	  };
 
 export const Iframe = (props: IGenericCustomFieldProps<IIframeSchema>) => {
 	// =========================================================================
@@ -25,6 +35,55 @@ export const Iframe = (props: IGenericCustomFieldProps<IIframeSchema>) => {
 	const promiseTimeoutRef = useRef<NodeJS.Timeout>();
 	const [dimensions, setDimensions] = useState<{ width?: number; height?: number }>({});
 	const { setFieldValidationConfig } = useValidationConfig();
+
+	// =========================================================================
+	// HELPER FUNCTIONS
+	// =========================================================================
+	const asyncValidation = async (value: unknown, isSubmit: boolean): Promise<boolean> => {
+		clearAsyncValidation();
+		iframePostMessage({ type: EPostMessageEvent.VALIDATE, payload: { value, isSubmit } });
+		try {
+			return await new Promise((resolve, reject) => {
+				deferredRef.current.resolve = resolve;
+				deferredRef.current.reject = reject;
+				promiseTimeoutRef.current = setTimeout(() => {
+					reject(false);
+					console.warn(
+						`validation timeout for iframe component: ${id}. does child iframe have the validation handler? eiher extend validationTimeout value of set to -1 to skip it.`
+					);
+				}, validationTimeout);
+			});
+		} catch (error) {
+			return false;
+		}
+	};
+
+	const clearAsyncValidation = () => {
+		if (promiseTimeoutRef.current) clearTimeout(promiseTimeoutRef.current);
+	};
+
+	const getTargetOriginFromSrc = useCallback(() => {
+		try {
+			const parsedUrl = new URL(src);
+			return `${parsedUrl.protocol}//${parsedUrl.host}`;
+		} catch (error) {
+			console.error("Invalid URL:", error);
+			return null;
+		}
+	}, [src]);
+
+	const iframePostMessage = useCallback(
+		(options: TIframePostMessageOptions) => {
+			const { type, payload } = options;
+			const targetOrigin = getTargetOriginFromSrc();
+			if (targetOrigin) {
+				iframeRef.current.contentWindow.postMessage({ type, payload }, targetOrigin);
+			} else {
+				console.warn(`invalid targetOrigin, iframe ${type} postMessage for ${id} aborted.`);
+			}
+		},
+		[getTargetOriginFromSrc, id]
+	);
 
 	// =========================================================================
 	// EFFECTS
@@ -50,9 +109,8 @@ export const Iframe = (props: IGenericCustomFieldProps<IIframeSchema>) => {
 	}, [validationTimeout]);
 
 	useEffect(() => {
-		iframePostMessage(EPostMessageEvent.SYNC, { error, id, value });
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [error, id, value]);
+		iframePostMessage({ type: EPostMessageEvent.SYNC, payload: { error, id, value } });
+	}, [error, id, value, iframePostMessage]);
 
 	// =========================================================================
 	// POSTMESSAGE HANDLERS
@@ -60,9 +118,8 @@ export const Iframe = (props: IGenericCustomFieldProps<IIframeSchema>) => {
 	useIframeMessage(
 		EPostMessageEvent.TRIGGER_SYNC,
 		useCallback(() => {
-			iframePostMessage(EPostMessageEvent.SYNC, { error, id, value });
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-		}, [error, id, value])
+			iframePostMessage({ type: EPostMessageEvent.SYNC, payload: { error, id, value } });
+		}, [error, id, value, iframePostMessage])
 	);
 
 	useIframeMessage<{ width?: number | undefined; height?: number | undefined }>(
@@ -77,10 +134,12 @@ export const Iframe = (props: IGenericCustomFieldProps<IIframeSchema>) => {
 
 	useIframeMessage<unknown>(
 		EPostMessageEvent.SET_VALUE,
-		useCallback((e) => {
-			formContext.setValue(id, e.data.payload, { shouldDirty: true });
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-		}, [])
+		useCallback(
+			(e) => {
+				formContext.setValue(id, e.data.payload, { shouldDirty: true });
+			},
+			[formContext, id]
+		)
 	);
 
 	useIframeMessage<boolean>(
@@ -94,45 +153,6 @@ export const Iframe = (props: IGenericCustomFieldProps<IIframeSchema>) => {
 			clearAsyncValidation();
 		}, [])
 	);
-	// =========================================================================
-	// HELPER FUNCTIONS
-	// =========================================================================
-	const asyncValidation = async (value: unknown, isSubmit: boolean): Promise<boolean> => {
-		clearAsyncValidation();
-		iframePostMessage(EPostMessageEvent.VALIDATE, { value, isSubmit });
-		try {
-			return await new Promise((resolve, reject) => {
-				deferredRef.current.resolve = resolve;
-				deferredRef.current.reject = reject;
-				promiseTimeoutRef.current = setTimeout(() => {
-					reject(false);
-					console.warn(
-						`validation timeout for iframe component: ${id}. does child iframe have the validation handler? eiher extend validationTimeout value of set to -1 to skip it.`
-					);
-				}, validationTimeout);
-			});
-		} catch (error) {
-			return false;
-		}
-	};
-
-	const clearAsyncValidation = () => {
-		if (promiseTimeoutRef.current) clearTimeout(promiseTimeoutRef.current);
-	};
-
-	const getTargetOriginFromSrc = () => {
-		try {
-			const parsedUrl = new URL(src);
-			return `${parsedUrl.protocol}//${parsedUrl.host}`;
-		} catch (error) {
-			console.error("Invalid URL:", error);
-			return "";
-		}
-	};
-
-	const iframePostMessage = (type: string, payload: unknown) => {
-		iframeRef.current.contentWindow.postMessage({ type, payload }, getTargetOriginFromSrc());
-	};
 
 	// =========================================================================
 	// RENDER FUNCTIONS
