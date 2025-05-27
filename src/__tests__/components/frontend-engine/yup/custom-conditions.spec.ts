44b52bc27993700440c46b5ef5a6d7f4d132eca6
Huy Pham Quoc <Quoc.huy.pham@ncs-sdc.com>
2025-05-27 15:19:16 +0800

    [CCUBE-1776][HUY] Resolve comments

--- src/__tests__/components/frontend-engine/yup/custom-conditions.spec.ts
+++ src/__tests__/components/frontend-engine/yup/custom-conditions.spec.ts
@@ -1,19 +1,18 @@
 import { YupHelper } from "../../../../context-providers";
 import { TestHelper } from "../../../../utils";
-
-const ERROR_MESSAGE = "test error message";
+import { ERROR_MESSAGE } from "../../../common";
 
 it.each`
 	type        | config              | valid                                                                               | invalid
 	${"uinfin"} | ${{ uinfin: true }} | ${["S1111111D", "T8017681Z", "F4769209K", "G5825195Q", "M1234567K", "", undefined]} | ${["S1234567A"]}
-	${"uen"}    | ${{ uen: true }}    | ${["200012345A", "12345678A", "T09LL0001B"]}                                        | ${["1234A567A", "T09L10001B"]}
+	${"uen"}    | ${{ uen: true }}    | ${["200012345A", "12345678A", "T09LL0001B", undefined]}                             | ${["1234A567A", "T09L10001B"]}
 `("should support $type validation", ({ config, valid, invalid }) => {
 	const schema = YupHelper.buildFieldSchema(YupHelper.mapSchemaType("string"), [
 		{ ...config, errorMessage: ERROR_MESSAGE },
 	]);
 
-	valid.forEach((validValue) => expect(() => schema.validateSync(validValue)).not.toThrowError());
-	invalid.forEach((invalidValue) =>
+	valid.forEach((validValue: string | undefined) => expect(() => schema.validateSync(validValue)).not.toThrow());
+	invalid.forEach((invalidValue: string | undefined) =>
 		expect(TestHelper.getError(() => schema.validateSync(invalidValue)).message).toBe(ERROR_MESSAGE)
 	);
 });
