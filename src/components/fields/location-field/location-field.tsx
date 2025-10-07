5015d20d8c96950e46963bfc67302e9fd0870c3e
jasmine.choy <jasmine.choy@ufinity.com>
2025-10-07 18:03:52 +0800

    Revert "[MOL-19548][JC] fix location text bar overflow when text is too long"
    This reverts commit 903e041851aa4aa72d6674996a2c9fe0a31a9a2b.

--- src/components/fields/location-field/location-field.tsx
+++ src/components/fields/location-field/location-field.tsx
@@ -9,7 +9,6 @@ import { ERROR_MESSAGES, StaticMap, Warning } from "../../shared";
 import { LocationHelper } from "./location-helper";
 import { LocationInput } from "./location-input";
 import { ILocationFieldSchema, ILocationFieldValues } from "./types";
-import { Wrapper } from "./location-field.styles";
 
 const LocationModal = lazy(() => import("./location-modal/location-modal"));
 
@@ -135,7 +134,7 @@ export const LocationField = (props: IGenericFieldProps<ILocationFieldSchema>) =
 	// RENDER FUNCTIONS
 	// =============================================================================
 	return (
-		<Wrapper id={id} data-testid={TestHelper.generateId(id)} className={className}>
+		<div id={id} data-testid={TestHelper.generateId(id)} className={className}>
 			<LocationInput
 				id={id}
 				label={formattedLabel}
@@ -198,6 +197,6 @@ export const LocationField = (props: IGenericFieldProps<ILocationFieldSchema>) =
 				)}
 			</Suspense>
 			<Warning id={id} message={warning} />
-		</Wrapper>
+		</div>
 	);
 };
