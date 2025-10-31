4cd39acb2ca8b29a7a4eaeed735f2990d8d5afa1
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2025-10-31 14:23:38 +0800

    Built package | Version: v1.19.15 | Gitlab: web-frontend-engine legacy/v1 13999462

---
+++ components/fields/e-signature-field/e-signature-field.styles.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+export declare const ESignatureWrapper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const ErrorWrapper: import("styled-components").StyledComponent<"h6", any, import("@lifesg/react-design-system/text").TextProps, never>;
+export declare const TryAgain: import("styled-components").StyledComponent<"button", any, any, never>;
+export declare const RefreshAlert: import("styled-components").StyledComponent<({ type, className, children, actionLink, actionLinkIcon, sizeType, showIcon, customIcon, maxCollapsedHeight, ...otherProps }: import("@lifesg/react-design-system/alert").AlertProps) => JSX.Element, any, {}, never>;
