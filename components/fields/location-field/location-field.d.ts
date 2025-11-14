5176d9ad3b6e76b427795efe92842385d31cb667
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2025-11-14 17:12:47 +0800

    Built package | Version: v1.19.16 | Gitlab: web-frontend-engine legacy/v1 14387325

---
+++ components/fields/location-field/location-field.d.ts
@@ -0,0 +1,3 @@
+import { IGenericFieldProps } from "..";
+import { ILocationFieldSchema } from "./types";
+export declare const LocationField: (props: IGenericFieldProps<ILocationFieldSchema>) => import("react/jsx-runtime").JSX.Element;
