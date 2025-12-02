1aac28317d4831941e32c88cbe0cb0ac0d38d1e5
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2025-12-02 20:54:54 +0800

    Built package | Version: v2.0.0-alpha.17 | Gitlab: web-frontend-engine main 14821404

---
+++ components/elements/text/types.d.ts
@@ -0,0 +1,18 @@
+import { TypographyProps } from "@lifesg/react-design-system/typography/types";
+import { V2_TextProps } from "@lifesg/react-design-system/v2_text/types";
+import { TComponentOmitProps } from "../../frontend-engine";
+import { IBaseElementSchema, TInlineElementSchema } from "../types";
+/**
+ * @deprecated Use design system v3 TTypographyType
+ */
+export type TTextType = "text-d1" | "text-d2" | "text-dbody" | "text-h1" | "text-h2" | "text-h3" | "text-h4" | "text-h5" | "text-h6" | "text-body" | "text-bodysmall" | "text-xsmall";
+/**
+ * @deprecated Use design system v3 ITypographySchema
+ */
+export interface ITextSchema extends IBaseElementSchema<TTextType>, TComponentOmitProps<V2_TextProps, "children"> {
+    children: string | string[] | Record<string, ITextSchema | TInlineElementSchema>;
+}
+export type TTypographyType = "heading-xxl" | "heading-xl" | "heading-md" | "heading-sm" | "heading-lg" | "heading-xs" | "body-md" | "body-sm" | "body-bl" | "body-xs";
+export interface ITypographySchema extends IBaseElementSchema<TTypographyType>, TComponentOmitProps<TypographyProps, "children"> {
+    children: string | string[] | Record<string, ITypographySchema | TInlineElementSchema>;
+}
--- src/custom-typings/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components";
-
-import { ThemeSpec } from "@lifesg/react-design-system/theme/types";
-
-declare module "styled-components" {
-	export interface DefaultTheme extends ThemeSpec {}
-}
