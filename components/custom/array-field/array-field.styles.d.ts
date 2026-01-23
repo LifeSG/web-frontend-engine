fe0e3cca64c67407bea620e91f2db829055266d3
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2026-01-23 14:10:09 +0800

    Built package | Version: v1.19.20 | Gitlab: web-frontend-engine legacy/v1 16057586

---
+++ components/custom/array-field/array-field.styles.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="react" />
+interface InsetStyleProps {
+    $inset?: string | number;
+}
+interface RemoveButtonStyleProps {
+    $alignment?: "left" | "right";
+}
+export declare const Inset: import("styled-components").StyledComponent<"div", any, InsetStyleProps, never>;
+export declare const SectionHeader: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const RemoveButton: import("styled-components").StyledComponent<(props: import("@lifesg/react-design-system/button-with-icon").ButtonWithIconProps & import("react").RefAttributes<HTMLButtonElement>) => import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>>, any, RemoveButtonStyleProps, never>;
+export declare const AddButton: import("styled-components").StyledComponent<(props: import("@lifesg/react-design-system/button-with-icon").ButtonWithIconProps & import("react").RefAttributes<HTMLButtonElement>) => import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>>, any, {}, never>;
+export declare const SectionDivider: import("styled-components").StyledComponent<({ thickness, lineStyle, layoutType, color, className, mobileCols, tabletCols, desktopCols, ...otherProps }: import("@lifesg/react-design-system/divider").DividerProps) => import("react/jsx-runtime").JSX.Element, any, {}, never>;
+export declare const WarningAlert: import("styled-components").StyledComponent<({ id, message, className }: import("../../shared").IWarningProps) => import("react/jsx-runtime").JSX.Element, any, {}, never>;
+export declare const CustomErrorDisplay: import("styled-components").StyledComponent<({ type, img, title, description, actionButton, additionalProps, imageOnly, illustrationScheme, ...otherProps }: import("@lifesg/react-design-system/error-display").ErrorDisplayProps) => JSX.Element, any, {}, never>;
+export {};
