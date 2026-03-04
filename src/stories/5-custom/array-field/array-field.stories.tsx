76157b5fe6293bf438f965b3493c12ebf7ff9ea7
Yip Jun Han <junhan.y_dcube@scientecpeople.com>
2026-03-05 02:38:16 +0800

    [BOOKINGSG-8773][JH] revert accidental commit

--- src/stories/5-custom/array-field/array-field.stories.tsx
+++ src/stories/5-custom/array-field/array-field.stories.tsx
@@ -239,7 +239,7 @@ InitialEntries.args = {
 	sectionTitle: "New fruit",
 	fieldSchema: SCHEMA,
 	initialEntries: 0,
-	validation: [{ length: 2 }],
+	validation: [{ min: 1 }, { max: 2 }],
 };
 
 export const DefaultValue = DefaultStoryTemplate<IArrayFieldSchema, object[]>("array-field-default-value").bind({});
@@ -248,7 +248,6 @@ DefaultValue.args = {
 	sectionTitle: "New fruit",
 	fieldSchema: SCHEMA,
 	defaultValues: [{ name: "Apple", colour: "Red" }, { name: "Berry" }],
-	validation: [{ length: 1 }],
 };
 DefaultValue.argTypes = {
 	defaultValues: {
