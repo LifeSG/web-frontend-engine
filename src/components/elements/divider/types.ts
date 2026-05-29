04733619c82bee770ffe0a5d203d87b0baa2ebf9
Ryan.Nguyen <trungnh41@fpt.com>
2026-05-29 15:55:50 +0700

    [CCUBE-2099][RYN] CleanupV2: divider v2 column

--- src/components/elements/divider/types.ts
+++ src/components/elements/divider/types.ts
@@ -2,8 +2,6 @@ import { DividerProps } from "@lifesg/react-design-system/divider";
 import { TComponentOmitProps } from "../../frontend-engine";
 import { IBaseElementSchema } from "../types";
 
-export interface IDividerSchema
-	extends IBaseElementSchema<"divider">,
-		TComponentOmitProps<DividerProps, "layoutType" | "desktopCols" | "tabletCols" | "mobileCols"> {
+export interface IDividerSchema extends IBaseElementSchema<"divider">, TComponentOmitProps<DividerProps> {
 	verticalMargin?: number | undefined;
 }
