217b8f2a888c3fa2ddd0d51ea2b9ac26f7f7888b
Ian Tjahjono <ian.t.gds@gt.tech.gov.sg>
2026-06-04 18:03:06 +0800

    [CCUBE-2101][IT] Remove post-build script and update rollup config to bundle css

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 import wyw from "@wyw-in-js/rollup";
+import generatePackageJson from "rollup-plugin-generate-package-json";
 
 const plugins = [
 	peerDepsExternal(), // Add the externals for me. [react, react-dom]
@@ -24,10 +25,25 @@ const plugins = [
 			exclude: ["**/stories/**", "**/__tests__/**", "**/__mocks__/**", "**/util/**"],
 		},
 	}),
-	postcss(),
+	postcss({
+		extract: true,
+		minimize: true,
+		sourceMap: true,
+	}),
 	image(),
 	json(),
 	terser(), // Helps remove comments, whitespace or logging codes
+	generatePackageJson({
+		outputFolder: "dist",
+		baseContents: (pkg) => ({
+			...pkg,
+			typings: "./index.d.ts",
+			main: "./cjs/index.js",
+			module: "./index.js",
+			style: "./index.css",
+			sideEffects: ["*.css"],
+		}),
+	}),
 ];
 
 export default {
@@ -39,15 +55,6 @@ export default {
 			sourcemap: true,
 			exports: "named",
 			chunkFileNames: "chunks/[name].[hash].js",
-			preserveModules: true,
-			preserveModulesRoot: "src",
-			interop: "compat",
-			entryFileNames: (chunkInfo) => {
-				if (chunkInfo.name.includes("node_modules")) {
-					return chunkInfo.name.replaceAll("node_modules", "external") + ".js";
-				}
-				return "[name].js";
-			},
 		},
 		{
 			dir: "dist/cjs",
@@ -55,15 +62,6 @@ export default {
 			sourcemap: true,
 			exports: "named",
 			chunkFileNames: "chunks/[name].[hash].js",
-			preserveModules: true,
-			preserveModulesRoot: "src",
-			interop: "compat",
-			entryFileNames: (chunkInfo) => {
-				if (chunkInfo.name.includes("node_modules")) {
-					return chunkInfo.name.replaceAll("node_modules", "external") + ".js";
-				}
-				return "[name].js";
-			},
 		},
 	],
 	plugins,
