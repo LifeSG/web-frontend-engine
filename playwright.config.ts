0bf385fa54ae6a27527bcd1bb92cf3e621320517
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-06-11 14:24:47 +0700

    [CCUBE-2136][GZ] connect playwright to docker container

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,15 +4,29 @@ const baseURL = process.env.E2E_BASE_URL ?? "http://localhost:3000";
 
 export default defineConfig({
 	testDir: "./e2e/tests",
+	/* Run tests in files in parallel */
 	fullyParallel: true,
+	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
+	/* Disable retries */
 	retries: 0,
+	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
+	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : [["list"], ["html"]],
 	snapshotPathTemplate: "{testDir}/{testFileDir}/__screenshots__/{projectName}/{testName}--{arg}{ext}",
+	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		baseURL,
+		/* Base URL to use in actions like `await page.goto('')`. */
+		baseURL: "http://host.docker.internal:3000",
+
+		/* Collect trace test fails. See https://playwright.dev/docs/trace-viewer */
 		trace: "retain-on-failure",
+
+		/* Connect to remote Playwright server running on Docker */
+		connectOptions: {
+			wsEndpoint: `ws://127.0.0.1:3010/`,
+		},
 	},
 	projects: [
 		{
