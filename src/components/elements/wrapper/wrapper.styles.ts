e728d7541d7a3dbefb65b332f4e61a750f18c910
ziggyzet <ziggy.z@gt.tech.gov.sg>
2026-06-25 15:22:38 +0700

    [CCUBE-2139][ZZ] refactor: migrate from styled components to linaria

--- src/components/elements/wrapper/wrapper.styles.ts
+++ src/components/elements/wrapper/wrapper.styles.ts
@@ -1,17 +1,12 @@
+import { css } from "@linaria/core";
 import { Colour, Font } from "@lifesg/react-design-system/theme";
-import styled from "styled-components";
-import { Sanitize } from "../../shared";
 
-export const StyledSublabel = styled(Sanitize)`
-	&.sub-label {
-		display: block;
-		${Font["body-md-regular"]};
-	}
+export const sublabel = css`
+	display: block;
+	${Font["body-md-regular"]};
 `;
 
-export const StyledHint = styled(Sanitize)`
-	&.label-hint {
-		color: ${Colour.text};
-		${Font["body-md-regular"]};
-	}
+export const hint = css`
+	color: ${Colour.text};
+	${Font["body-md-regular"]};
 `;
