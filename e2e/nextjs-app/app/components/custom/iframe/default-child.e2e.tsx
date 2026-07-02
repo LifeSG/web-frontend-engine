b440ba94e9c2788e59b431bed32f20cb6f0b4361
Ian Tjahjono <ian.t.gds@gt.tech.gov.sg>
2026-07-02 15:26:10 +0800

    [CCUBE-2168][IT] Update input component with label to remove hydration errors

--- e2e/nextjs-app/app/components/custom/iframe/default-child.e2e.tsx
+++ e2e/nextjs-app/app/components/custom/iframe/default-child.e2e.tsx
@@ -2,22 +2,11 @@
 
 import { Button } from "@lifesg/react-design-system/button";
 import { Form } from "@lifesg/react-design-system/form";
-import { useEffect, useState } from "react";
 
 export default function IframeDefaultChildPage() {
-	const [mounted, setMounted] = useState(false);
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	if (!mounted) {
-		return null;
-	}
-
 	return (
 		<div>
-			<Form.Input label="This is inside the iframe" />
+			<Form.Label>This is inside the iframe</Form.Label>
 			<Button type="submit">Submit</Button>
 		</div>
 	);
