a74b856982791e7fa690ea9c4aaad94c4d254ea6
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-07-03 14:06:10 +0800

    [CCUBE-2139][RL] Work around css ordering issue

--- e2e/nextjs-app/app/components/elements/wrapper/column-layout.e2e.tsx
+++ e2e/nextjs-app/app/components/elements/wrapper/column-layout.e2e.tsx
@@ -6,26 +6,22 @@ const COLUMN_LAYOUT_SCHEMA: IFrontendEngineData = {
 	sections: {
 		section: {
 			uiType: "section",
+			layoutType: "grid",
 			children: {
-				grid: {
-					uiType: "grid",
-					children: {
-						field1: {
-							uiType: "text-field",
-							label: "First name",
-							columns: { xxs: 8, sm: 12, xl: 6 },
-						},
-						field2: {
-							uiType: "text-field",
-							label: "Last name",
-							columns: { xxs: 8, sm: 12, xl: 6 },
-						},
-						field3: {
-							uiType: "text-field",
-							label: "Full width field",
-							columns: { xxs: 8, sm: 12, xl: 12 },
-						},
-					},
+				field1: {
+					uiType: "text-field",
+					label: "First name",
+					columns: { xxs: 8, sm: 12, xl: 6 },
+				},
+				field2: {
+					uiType: "text-field",
+					label: "Last name",
+					columns: { xxs: 8, sm: 12, xl: 6 },
+				},
+				field3: {
+					uiType: "text-field",
+					label: "Full width field",
+					columns: { xxs: 8, sm: 12, xl: 12 },
 				},
 			},
 		},
