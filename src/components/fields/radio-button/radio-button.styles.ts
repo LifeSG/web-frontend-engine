10ca0258cd69bd5557ebe4ee050d48f4306558b2
Ian Tjahjono <ian.t.gds@gt.tech.gov.sg>
2026-07-03 17:31:25 +0800

    [CCUBE-2108][IT] Convert styled components to linaria

--- src/components/fields/radio-button/radio-button.styles.ts
+++ src/components/fields/radio-button/radio-button.styles.ts
@@ -1,65 +1,54 @@
-import { ImageButton } from "@lifesg/react-design-system/image-button";
-import { RadioButton } from "@lifesg/react-design-system/radio-button";
-import { Toggle } from "@lifesg/react-design-system/toggle";
-import { Typography } from "@lifesg/react-design-system/typography";
-import styled from "styled-components";
-import { TRadioToggleLayoutType } from "./types";
+import { css } from "@linaria/core";
 import { Spacing } from "@lifesg/react-design-system/theme";
 
-interface ILabelProps {
-	disabled?: boolean | undefined;
-}
-
-interface IToggleWrapperProps {
-	$layoutType?: TRadioToggleLayoutType;
-}
-
-export const Label = styled(Typography.BodyMD)<ILabelProps>`
+export const label = css`
 	cursor: pointer;
-
-	&.labelDisabled {
-		cursor: not-allowed;
-	}
 `;
 
-export const StyledRadioButton = styled(RadioButton)`
+export const labelDisabled = css`
+	cursor: not-allowed;
+`;
+
+export const styledRadioButton = css`
 	margin-right: ${Spacing["spacing-4"]};
 	flex-shrink: 0;
 `;
 
-export const StyledImageButton = styled(ImageButton)`
+export const styledImageButton = css`
 	flex: 1;
+
 	img {
 		min-width: 3.5rem;
 	}
 `;
 
-export const RadioContainer = styled.div`
+export const radioContainer = css`
 	display: flex;
 	align-items: center;
+
 	&:not(:last-of-type) {
 		margin-bottom: ${Spacing["spacing-16"]};
 	}
 `;
 
-export const FlexImageWrapper = styled.div`
+export const flexImageWrapper = css`
 	display: flex;
 	flex-wrap: wrap;
 	gap: ${Spacing["spacing-16"]};
 `;
 
-export const FlexToggleWrapper = styled.div<IToggleWrapperProps>`
+export const flexToggleWrapper = css`
 	display: flex;
 	flex-direction: row;
 	flex-wrap: wrap;
 	gap: ${Spacing["spacing-16"]};
-
-	&.flexToggleWrapperVertical {
-		flex-direction: column;
-	}
 `;
 
-export const StyledToggle = styled(Toggle)`
+export const flexToggleWrapperVertical = css`
+	flex-direction: column;
+`;
+
+export const styledToggle = css`
 	[data-id="toggle-composite-children"] {
 		margin: 0;
 		padding: 0;
