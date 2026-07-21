503ef922c144e35682525b45be562c553de2b055
Ryan.Nguyen <trungnh41@fpt.com>
2026-07-21 23:37:38 +0700

    [CCUBE-2169][RYN] add e2e tests and snapshots

--- e2e/tests/components/custom/filter/filter.e2e.spec.ts
+++ e2e/tests/components/custom/filter/filter.e2e.spec.ts
@@ -1,4 +1,4 @@
-import { forComponent, test } from "../../../utils/fixtures";
+import { expect, forComponent, test } from "../../../utils/fixtures";
 
 const withStory = forComponent("custom/filter");
 
@@ -13,6 +13,54 @@ test.describe("Filter", () => {
 		test("Default rendering", async ({ story }) => {
 			await story.goto();
 			await story.snapshot("default");
+		});
+	});
+
+	test.describe(() => {
+		test.use({
+			storyOptions: {
+				...withStory("with-text-content"),
+			},
+		});
+
+		test("Plain text content", async ({ story }) => {
+			await story.goto();
+			await story.snapshot("mount");
+		});
+	});
+
+	test.describe(() => {
+		test.use({
+			storyOptions: {
+				...withStory("with-filter-checkbox"),
+			},
+		});
+
+		test("Filter checkbox", async ({ story }) => {
+			await story.goto();
+			await story.snapshot("mount");
+		});
+	});
+
+	test.describe(() => {
+		test.use({
+			storyOptions: {
+				...withStory("with-tooltip"),
+			},
+		});
+
+		test("Filter title with tooltip", async ({ story }) => {
+			await story.goto();
+
+			await test.step("Renders tooltip icon in filter title", async () => {
+				await expect(story.page.getByTestId("filterItem-popover")).toBeVisible();
+				await story.snapshot("mount");
+			});
+
+			await test.step("Shows popover content on click", async () => {
+				await story.page.getByTestId("filterItem-popover").click();
+				await story.snapshot("popover-open");
+			});
 		});
 	});
 });
