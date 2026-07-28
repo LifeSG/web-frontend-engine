337e4cdb3d46c44228eff561c68d61d0e2fccf2c
Ian Tjahjono <ian.t.gds@gt.tech.gov.sg>
2026-07-28 16:58:12 +0800

    [CCUBE-2203][IT] Add e2e tests and screenshots

---
+++ e2e/tests/components/fields/multi-select/multi-select.e2e.spec.ts
@@ -0,0 +1,36 @@
+import { createStoryTest, expect } from "../../../utils/fixtures";
+
+const createMultiSelectTest = (story: string) =>
+	createStoryTest({
+		component: "fields/multi-select",
+		story,
+		createLocators: (page) => ({
+			selector: page.getByTestId("selector"),
+			dropdown: page.getByTestId("dropdown-list"),
+		}),
+	});
+
+const defaultTest = createMultiSelectTest("default");
+const warningTest = createMultiSelectTest("warning");
+
+defaultTest.describe(() => {
+	defaultTest("Default", async ({ story }) => {
+		await story.goto();
+		await story.snapshot("mount");
+	});
+
+	defaultTest("Dropdown", async ({ story }) => {
+		await story.goto();
+
+		await story.locators.selector.click();
+		await expect(story.locators.dropdown).toBeVisible();
+		await story.snapshot("open", { fullscreen: true });
+	});
+});
+
+warningTest.describe(() => {
+	warningTest("Warning", async ({ story }) => {
+		await story.goto();
+		await story.snapshot("mount");
+	});
+});
