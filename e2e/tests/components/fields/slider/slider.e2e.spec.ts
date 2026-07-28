416272440fc47e091424b8b0d5282b38e7fe9278
Ryan.Nguyen <trungnh41@fpt.com>
2026-07-28 13:36:57 +0700

    [CCUBE-2209][RYN] Slider: update e2e tests on DS bump

--- e2e/tests/components/fields/slider/slider.e2e.spec.ts
+++ e2e/tests/components/fields/slider/slider.e2e.spec.ts
@@ -28,33 +28,4 @@ test.describe("Slider", () => {
 			await story.snapshot("mount");
 		});
 	});
-
-	test.describe(() => {
-		test.use({
-			storyOptions: {
-				...withStory("drag"),
-			},
-		});
-
-		test("Drag", async ({ story }) => {
-			await story.goto();
-
-			const thumb = story.page.getByRole("slider");
-			const track = story.page.getByTestId("field__slider");
-
-			const trackBox = await track.boundingBox();
-			const thumbBox = await thumb.boundingBox();
-
-			const startX = thumbBox.x + thumbBox.width / 2;
-			const startY = thumbBox.y + thumbBox.height / 2;
-			const targetX = trackBox.x + trackBox.width * 0.25;
-
-			await story.page.mouse.move(startX, startY);
-			await story.page.mouse.down();
-			await story.page.mouse.move(targetX, startY, { steps: 10 });
-			await story.page.mouse.up();
-
-			await story.snapshot("dragged");
-		});
-	});
 });
