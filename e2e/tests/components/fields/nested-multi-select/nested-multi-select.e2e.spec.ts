a1a101d895ecd8f2a9dc99fc5319e594c3b59734
ziggyzet <ziggy.z@gt.tech.gov.sg>
2026-07-29 11:33:45 +0700

    [CCUBE-2204] test: functional test for nested multi select

---
+++ e2e/tests/components/fields/nested-multi-select/nested-multi-select.e2e.spec.ts
@@ -0,0 +1,33 @@
+import { createStoryTest } from "../../../utils/fixtures";
+
+const createNestedMultiSelectTest = (story: string) =>
+	createStoryTest({
+		component: "fields/nested-multi-select",
+		story,
+		createLocators: (page) => ({
+			trigger: page.getByTestId("selector"),
+		}),
+	});
+
+const defaultTest = createNestedMultiSelectTest("default");
+const warningTest = createNestedMultiSelectTest("warning");
+
+defaultTest.describe(() => {
+	defaultTest("Default rendering", async ({ story }) => {
+		await story.goto();
+		await story.snapshot("mount");
+	});
+
+	defaultTest("Dropdown open", async ({ story }) => {
+		await story.goto();
+		await story.locators.trigger.click();
+		await story.snapshot("open", { fullscreen: true });
+	});
+});
+
+warningTest.describe(() => {
+	warningTest("Warning state", async ({ story }) => {
+		await story.goto();
+		await story.snapshot("mount");
+	});
+});
