b90dbe4b345311c29bd4b9816802f5dba03b0308
Ryan Nguyen <trungnh41@gt.tech.gov.sg>
2026-07-29 13:00:22 +0700

    [CCUBE-2208][RYN] SelectHistogram: add basic e2e tests (#699)
    Co-authored-by: Ryan.Nguyen <trungnh41@fpt.com>

---
+++ e2e/nextjs-app/app/components/fields/select-histogram/default.e2e.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { FrontendEngine, IFrontendEngineData } from "@lifesg/web-frontend-engine";
+
+const SCHEMA: IFrontendEngineData = {
+	sections: {
+		section: {
+			uiType: "section",
+			children: {
+				field: {
+					uiType: "select-histogram",
+					label: "Price of fruits",
+					histogramSlider: {
+						bins: [
+							{ minValue: 0, count: 0 },
+							{ minValue: 10, count: 35 },
+							{ minValue: 20, count: 15 },
+							{ minValue: 50, count: 20 },
+							{ minValue: 70, count: 40 },
+							{ minValue: 90, count: 50 },
+						],
+						interval: 10,
+					},
+				},
+			},
+		},
+	},
+};
+
+export default function SelectHistogramPage() {
+	return <FrontendEngine data={SCHEMA} />;
+}
