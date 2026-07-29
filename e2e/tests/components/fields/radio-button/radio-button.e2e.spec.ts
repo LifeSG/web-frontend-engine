8c650a44f093531e6352ad23906d82d46e45e0b9
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-07-29 09:36:25 +0800

    [MISC][RL] Mock image button third party image to reduce flake

--- e2e/tests/components/fields/radio-button/radio-button.e2e.spec.ts
+++ e2e/tests/components/fields/radio-button/radio-button.e2e.spec.ts
@@ -1,4 +1,5 @@
 import { forComponent, test } from "../../../utils/fixtures";
+import { createSolidColorPng } from "../../../utils/image-fixtures";
 
 const withStory = forComponent("fields/radio-button");
 
@@ -93,6 +94,23 @@ test.describe("Radio Image Button", () => {
 		});
 
 		test("Default", async ({ story }) => {
+			await story.page.route("/image/*/*/*.png", async (route) => {
+				const url = new URL(route.request().url());
+				const [x, y, zVal] = url.pathname.split("/").slice(-3);
+				const z = zVal.split(".")[0];
+
+				const r = Number(z) % 256;
+				const g = Number(x) % 256;
+				const b = Number(y) % 256;
+				const image = createSolidColorPng(256, 192, r, g, b);
+
+				await route.fulfill({
+					status: 200,
+					contentType: "image/png",
+					body: image,
+				});
+			});
+
 			await story.goto();
 			await story.snapshot("image-button-default");
 		});
