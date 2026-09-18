3509eaccd12e7291cacf8276c496bf3052d7a9b3
Chen Shengxi (Dave) <shengxi@cmc-apac.sg>
2026-09-18 18:30:55 +0800

    [MOL-22453][SX] FilterCheckbox: omit sanitizeOptions (equivalent to default)

--- src/components/custom/filter/filter-checkbox/filter-checkbox.tsx
+++ src/components/custom/filter/filter-checkbox/filter-checkbox.tsx
@@ -2,7 +2,6 @@ import { Filter } from "@lifesg/react-design-system/filter";
 import { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import useDeepCompareEffect from "use-deep-compare-effect";
-import sanitizeHtml from "sanitize-html";
 import { TestHelper } from "../../../../utils";
 import { Sanitize } from "../../../shared";
 import { IGenericCustomFieldProps } from "../../types";
@@ -62,7 +61,7 @@ export const FilterCheckbox = (props: IGenericCustomFieldProps<IFilterCheckboxSc
 			onExpandChange={setExpandedState}
 			onSelect={handleChange}
 			labelExtractor={(item) => (
-				<Sanitize inline sanitizeOptions={{ allowedAttributes: sanitizeHtml.defaults.allowedAttributes }}>
+				<Sanitize inline>
 					{item.label}
 				</Sanitize>
 			)}
