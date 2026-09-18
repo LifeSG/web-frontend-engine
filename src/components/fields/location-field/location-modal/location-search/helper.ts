c36cb8837f1ce72f7d70811d7b3d1ad5ea6c6d98
Chen Shengxi (Dave) <shengxi@cmc-apac.sg>
2026-09-18 14:21:56 +0800

    [MOL-22453][SX] LocationField: escape search query before use in RegExp (prevent ReDoS)

--- src/components/fields/location-field/location-modal/location-search/helper.ts
+++ src/components/fields/location-field/location-modal/location-search/helper.ts
@@ -4,8 +4,15 @@ export const pagination = <T>(array: T[], pageSize: number, pageNum: number) => 
 	return array.slice((pageNum - 1) * pageSize, pageNum * pageSize);
 };
 
+const escapeRegExp = (value: string): string => value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
 export const boldResultsWithQuery = (arr: IResultListItem[], query: string) => {
-	const regex = new RegExp(query, "gi");
+	let regex: RegExp;
+	try {
+		regex = new RegExp(escapeRegExp(query), "gi");
+	} catch {
+		return arr;
+	}
 	return arr.map((obj) => {
 		const newAddress = (obj.displayAddressText || obj.address).replace(
 			regex,
